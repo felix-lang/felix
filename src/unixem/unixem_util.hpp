96e401acd25f02440b1ecab065083b7483fbe940
skaller <skaller@3fca3a5c-071c-0410-b500-ba939811c8f2>
2007-11-11 00:43:09 +0000

    xfile glob
    git-svn-id: https://felix.svn.sourceforge.net/svnroot/felix/felix/trunk@1914 3fca3a5c-071c-0410-b500-ba939811c8f2

---
+++ src/unixem/unixem_util.hpp
@@ -0,0 +1,10 @@
+#line 101 "../lpsrc/flx_glob.pak"
+#ifndef __UNIXEM_UTIL_
+#define __UNIXEM_UTIL_
+
+int errno_from_Win32(unsigned long w32Err);
+char get_current_drive(void);
+# define UNIXEM_NUM_ELEMENTS(x)        (sizeof(x) / sizeof(0[x]))
+#endif
+
+
