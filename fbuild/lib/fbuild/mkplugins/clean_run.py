314575118ddc3ecff82ecad52e44e1120825e32c
skaller <skaller@3fca3a5c-071c-0410-b500-ba939811c8f2>
2007-12-07 00:47:34 +0000

    Add a plugin that wipes out all the test stamps, to force re-running ALL the tests. A bit hacky .. the --force option should do that perhaps but doesn't. Well this thing works, so allows a complete re-run of all the tests.
    git-svn-id: https://felix.svn.sourceforge.net/svnroot/felix/felix/trunk@2098 3fca3a5c-071c-0410-b500-ba939811c8f2

---
+++ fbuild/lib/fbuild/mkplugins/clean_run.py
@@ -0,0 +1,30 @@
+import os
+import glob
+
+from fbuild.flxbuild.process import Process
+from fbuild.flxbuild.flxutil import erasefile
+
+import config
+
+# cleans products, but not extracted sources
+class clean_run(Process):
+  help = 'remove generated C++ and binaries from test locations'
+
+  def runme(self, *args):
+    for d in glob.glob(os.path.join("pkg-stamps", "*.run")):
+      print "Removing " + d
+      erasefile(d)
+    for d in glob.glob(os.path.join("pkg-stamps", "*.test")):
+      print "Removing " + d
+      erasefile(d)
+    for d in glob.glob(os.path.join("pkg-stamps", "*","*.test")):
+      print "Removing " + d
+      erasefile(d)
+    for d in glob.glob(os.path.join("pkg-stamps", "*","*","*.test")):
+      print "Removing " + d
+      erasefile(d)
+    for d in glob.glob(os.path.join("pkg-stamps", "*","*","*","*.test")):
+      print "Removing " + d
+      erasefile(d)
+    exit(0)
+      
