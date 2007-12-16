9f6841225b22977a232e88a158e48db909b28914
idadesub <idadesub@3fca3a5c-071c-0410-b500-ba939811c8f2>
2007-12-16 11:12:46 +0000

    Indent path.py to 4 spaces.
    git-svn-id: https://felix.svn.sourceforge.net/svnroot/felix/felix/trunk@2116 3fca3a5c-071c-0410-b500-ba939811c8f2

--- fbuild/lib/fbuild/path.py
+++ fbuild/lib/fbuild/path.py
@@ -4,59 +4,59 @@ import glob
 import types
 
 def splitall(path):
-  paths = []
-  old_path = path
+    paths = []
+    old_path = path
 
-  while True:
-    path, filename = os.path.split(path)
+    while True:
+        path, filename = os.path.split(path)
 
-    if path == old_path:
-      if path:
-        paths.append(path)
-      break
-    else:
-      old_path = path
-      paths.append(filename)
-  paths.reverse()
-  return paths
+        if path == old_path:
+            if path:
+                paths.append(path)
+            break
+        else:
+            old_path = path
+            paths.append(filename)
+    paths.reverse()
+    return paths
 
 def relativepath(root, path):
-  roots = splitall(os.path.abspath(root))
-  paths = splitall(os.path.abspath(path))
+    roots = splitall(os.path.abspath(root))
+    paths = splitall(os.path.abspath(path))
 
-  for i, (r, p) in enumerate(zip(roots, paths)):
-    j = i
-    if r != p:
-      break
-  else:
-    i += 1
-    j = len(roots)
+    for i, (r, p) in enumerate(zip(roots, paths)):
+        j = i
+        if r != p:
+            break
+    else:
+        i += 1
+        j = len(roots)
 
-  new_paths = ['..'] * (len(roots) - i) + paths[j:]
+    new_paths = ['..'] * (len(roots) - i) + paths[j:]
 
-  if not new_paths:
-    return '.'
-  else:
-    return os.path.join(*new_paths)
+    if not new_paths:
+        return '.'
+    else:
+        return os.path.join(*new_paths)
 
 def find(path, name=None, include_dirs=True):
-  for root, dirs, files in os.walk(path):
-    if include_dirs:
-      files += dirs
+    for root, dirs, files in os.walk(path):
+        if include_dirs:
+            files += dirs
 
-    for f in files:
-      if name is not None and not fnmatch.fnmatch(f, name):
-        continue
+        for f in files:
+            if name is not None and not fnmatch.fnmatch(f, name):
+                continue
 
-      yield os.path.join(root, f)
+            yield os.path.join(root, f)
 
 def glob_paths(paths, root='.'):
-  new_paths = []
-  for path in paths:
-    # if the path is not a string, assume it's a list of path elements
-    if not isinstance(path, types.StringTypes):
-      path = os.path.join(*path)
+    new_paths = []
+    for path in paths:
+        # if the path is not a string, assume it's a list of path elements
+        if not isinstance(path, types.StringTypes):
+            path = os.path.join(*path)
 
-    pattern = os.path.join(root, path)
-    new_paths.extend(glob.glob(pattern))
-  return new_paths
+        pattern = os.path.join(root, path)
+        new_paths.extend(glob.glob(pattern))
+    return new_paths
