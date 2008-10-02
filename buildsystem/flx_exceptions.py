8b8936a31074ec728d420bf3133d8c5d44a24b82
idadesub <idadesub@3fca3a5c-071c-0410-b500-ba939811c8f2>
2008-10-02 08:23:59 +0000

    Rewrite flx_exceptions to use the builder-based fbuild.
    git-svn-id: https://felix.svn.sourceforge.net/svnroot/felix/felix/trunk@2248 3fca3a5c-071c-0410-b500-ba939811c8f2

--- buildsystem/flx_exceptions.py
+++ buildsystem/flx_exceptions.py
@@ -1,20 +1,21 @@
 import fbuild
-import fbuild.packages
-import fbuild.packages.cxx as cxx
 from fbuild.path import Path
+
+import buildsystem
 
 # -----------------------------------------------------------------------------
 
 def build_runtime(phase):
     path = Path('src/exceptions')
 
-    for hpp in (
-            fbuild.buildroot / 'config/target/flx_exceptions_config.hpp',
-            path / 'flx_exceptions.hpp'):
-        fbuild.packages.Copy(fbuild.buildroot / 'lib/rtl', hpp).build()
+    buildsystem.copy_hpps_to_rtl(
+        fbuild.buildroot / 'config/target/flx_exceptions_config.hpp',
+        path / 'flx_exceptions.hpp',
+    )
 
-    return cxx.SharedLibrary(fbuild.buildroot / 'lib/rtl/flx_exceptions_dynamic',
+    return phase.cxx.shared.build_lib(
+        dst=fbuild.buildroot / 'lib/rtl/flx_exceptions_dynamic',
         srcs=[path / 'flx_exceptions.cpp'],
         includes=[fbuild.buildroot / 'config/target'],
         macros=['BUILD_EXCEPTIONS'],
-        builder=phase.cxx)
+    )
