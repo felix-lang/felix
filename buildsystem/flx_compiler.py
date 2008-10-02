97971de140dedec04b1b7d31b3e56655c2757ee0
idadesub <idadesub@3fca3a5c-071c-0410-b500-ba939811c8f2>
2008-10-02 08:26:17 +0000

    Rewrite flx_compiler to use the builder-based fbuild.
    git-svn-id: https://felix.svn.sourceforge.net/svnroot/felix/felix/trunk@2257 3fca3a5c-071c-0410-b500-ba939811c8f2

--- buildsystem/flx_compiler.py
+++ buildsystem/flx_compiler.py
@@ -1,244 +1,197 @@
 import fbuild
-import fbuild.packages.ocaml as ocaml
 from fbuild.path import Path
 from fbuild.record import Record
 
-from buildsystem.dyp import Dypgen
-
 # -----------------------------------------------------------------------------
 
-def build_flx_misc(builder):
+def build_flx_misc(ocaml):
     path = Path('src/compiler/flx_misc')
-    return ocaml.Library(path/'flx_misc',
+    return ocaml.builder.build_lib(path / 'flx_misc',
         srcs=[path / '*.ml{,i}'],
-        libs=['nums', 'str', 'unix'],
-        builder=builder)
+        libs=['nums', 'str', 'unix'])
 
-def build_flx_core(builder):
+def build_flx_core(ocaml):
     path = Path('src/compiler/flx_core')
-    return ocaml.Library(path / 'flx_core',
+    return ocaml.builder.build_lib(path / 'flx_core',
         srcs=[path / '*.ml{,i}'],
         libs=[
             'nums',
-            fbuild.env.run(build_flx_misc, builder),
-            fbuild.env.run('buildsystem.ocs.build', builder).lib,
-        ],
-        builder=builder)
+            fbuild.env.run(build_flx_misc, ocaml),
+            fbuild.env.run('buildsystem.ocs.build_lib', ocaml),
+        ])
 
-def build_flx_version(builder):
+def build_flx_version(ocaml):
     path = fbuild.buildroot / 'src/compiler/flx_version'
-    return ocaml.Library(path / 'flx_version',
-        srcs=[path / '*.ml{,i}'],
-        builder=builder)
+    return ocaml.builder.build_lib(path / 'flx_version',
+        srcs=[path / '*.ml{,i}'])
 
-def build_flx_version_hook(builder):
+def build_flx_version_hook(ocaml):
     path = fbuild.buildroot / 'src/compiler/flx_version_hook'
-    return ocaml.Library(path / 'flx_version_hook',
+    return ocaml.builder.build_lib(path / 'flx_version_hook',
         srcs=[path / '*.ml{,i}'],
-        libs=[fbuild.env.run(build_flx_version, builder)],
-        builder=builder)
+        libs=[fbuild.env.run(build_flx_version, ocaml)])
 
-def build_flx_lex(builder):
+def build_flx_lex(ocaml):
     path = Path('src/compiler/flx_lex')
-    return ocaml.Library(path/'flx_lex',
+    dypgen = fbuild.env.run('buildsystem.dyp.build_dypgen', ocaml)
+    return ocaml.builder.build_lib(path/'flx_lex',
         srcs=[
             path / '*.ml{,i}',
             fbuild.buildroot/ path / '*.ml{,i}',
-            ocaml.Ocamllex(fbuild.buildroot / path / 'flx_lex.mll'),
-            Dypgen(fbuild.buildroot / path / 'flx_preparse.dyp',
+            ocaml.ocamllex(fbuild.buildroot / path / 'flx_lex.mll'),
+            dypgen(fbuild.buildroot / path / 'flx_preparse.dyp',
                 flags=['--prio-pt', '--pv-obj', '--noemit-token-type']),
         ],
         libs=[
-            fbuild.env.run('buildsystem.dyp.build_dyplib', builder),
-            fbuild.env.run('buildsystem.ocs.build', builder).lib,
-            fbuild.env.run('buildsystem.sex.build', builder),
-            fbuild.env.run(build_flx_misc, builder),
-            fbuild.env.run(build_flx_core, builder),
-            fbuild.env.run(build_flx_version, builder),
-        ],
-        builder=builder)
+            fbuild.env.run('buildsystem.dyp.build_lib', ocaml),
+            fbuild.env.run('buildsystem.ocs.build_lib', ocaml),
+            fbuild.env.run('buildsystem.sex.build', ocaml),
+            fbuild.env.run(build_flx_misc, ocaml),
+            fbuild.env.run(build_flx_core, ocaml),
+            fbuild.env.run(build_flx_version, ocaml),
+        ])
 
-def build_flx_parse(builder):
+def build_flx_parse(ocaml):
     path = Path('src/compiler/flx_parse')
-    return ocaml.Library(path/'flx_parse',
+    dypgen = fbuild.env.run('buildsystem.dyp.build_dypgen', ocaml)
+    return ocaml.builder.build_lib(path/'flx_parse',
         srcs=[
             path / '*.ml{,i}',
-            Dypgen(fbuild.buildroot / path / 'flx_parse.dyp',
+            dypgen(fbuild.buildroot / path / 'flx_parse.dyp',
                 flags=['--prio-pt', '--pv-obj', '--noemit-token-type']),
         ],
         libs=[
-            fbuild.env.run('buildsystem.dyp.build_dyplib', builder),
-            fbuild.env.run('buildsystem.ocs.build', builder).lib,
-            fbuild.env.run('buildsystem.sex.build', builder),
-            fbuild.env.run(build_flx_misc, builder),
-            fbuild.env.run(build_flx_core, builder),
-            fbuild.env.run(build_flx_version, builder),
-            fbuild.env.run(build_flx_lex, builder),
-        ],
-        builder=builder)
+            fbuild.env.run('buildsystem.dyp.build_lib', ocaml),
+            fbuild.env.run('buildsystem.ocs.build_lib', ocaml),
+            fbuild.env.run('buildsystem.sex.build', ocaml),
+            fbuild.env.run(build_flx_misc, ocaml),
+            fbuild.env.run(build_flx_core, ocaml),
+            fbuild.env.run(build_flx_version, ocaml),
+            fbuild.env.run(build_flx_lex, ocaml),
+        ])
 
-def build_flxcclib(builder):
+def build_flxcclib(ocaml):
     path = fbuild.buildroot / 'src/compiler/flxcclib'
-    return ocaml.Library(path / 'flxcclib',
-        srcs=[path / '*.ml{,i}'],
+    return ocaml.builder.build_lib(path / 'flxcclib', [path / '*.ml{,i}'],
+        includes=[fbuild.buildroot / 'src/compiler/cil/src'],
         libs=[
-            fbuild.env.run('buildsystem.cil.build', builder),
-            fbuild.env.run(build_flx_core, builder),
-        ],
-        builder=builder)
+            fbuild.env.run('buildsystem.cil.build', ocaml),
+            fbuild.env.run(build_flx_core, ocaml),
+        ])
 
-def build_flx_desugar(builder):
+def build_flx_desugar(ocaml):
     path = Path('src/compiler/flx_desugar')
-    return ocaml.Library(path / 'flx_desugar',
-        srcs=[path / '*.ml{,i}'],
+
+    return ocaml.builder.build_lib(path / 'flx_desugar', [path / '*.ml{,i}'],
+        includes=[
+            fbuild.buildroot / 'src/compiler/cil/ocamlutil',
+            fbuild.buildroot / 'src/compiler/cil/src',
+            fbuild.buildroot / 'src/compiler/cil/src/frontc',
+        ],
         libs=[
             'nums',
             'unix',
-            fbuild.env.run('buildsystem.dyp.build_dyplib', builder),
-            fbuild.env.run('buildsystem.ocs.build', builder).lib,
-            fbuild.env.run('buildsystem.sex.build', builder),
-            fbuild.env.run('buildsystem.cil.build', builder),
-            fbuild.env.run(build_flx_misc, builder),
-            fbuild.env.run(build_flx_core, builder),
-            fbuild.env.run(build_flx_version, builder),
-            fbuild.env.run(build_flx_lex, builder),
-            fbuild.env.run(build_flx_parse, builder),
-            fbuild.env.run(build_flxcclib, builder),
-        ],
-        builder=builder)
+            fbuild.env.run('buildsystem.dyp.build_lib', ocaml),
+            fbuild.env.run('buildsystem.ocs.build_lib', ocaml),
+            fbuild.env.run('buildsystem.sex.build', ocaml),
+            fbuild.env.run('buildsystem.cil.build', ocaml),
+            fbuild.env.run(build_flx_misc, ocaml),
+            fbuild.env.run(build_flx_core, ocaml),
+            fbuild.env.run(build_flx_version, ocaml),
+            fbuild.env.run(build_flx_lex, ocaml),
+            fbuild.env.run(build_flx_parse, ocaml),
+            fbuild.env.run(build_flxcclib, ocaml),
+        ])
 
-def build_inria_re(builder):
+def build_inria_re(ocaml):
     path = Path('src/compiler/inria_re')
-    return ocaml.Library(path / 'inria_re', [path / '*.ml{,i}'],
-        builder=builder)
+    return ocaml.builder.build_lib(path / 'inria_re', [path / '*.ml{,i}'])
 
-def build_flx_bind(builder):
+def build_flx_bind(ocaml):
     path = Path('src/compiler/flx_bind')
-    return ocaml.Library(path / 'flx_bind', [path / '*.ml{,i}'],
+    return ocaml.builder.build_lib(path / 'flx_bind', [path / '*.ml{,i}'],
         libs=[
             'nums',
-            fbuild.env.run(build_flx_misc, builder),
-            fbuild.env.run(build_flx_core, builder),
-            fbuild.env.run(build_inria_re, builder),
-        ],
-        builder=builder)
+            fbuild.env.run(build_flx_misc, ocaml),
+            fbuild.env.run(build_flx_core, ocaml),
+            fbuild.env.run(build_inria_re, ocaml),
+        ])
 
-def build_flx_frontend(builder):
+def build_flx_frontend(ocaml):
     path = Path('src/compiler/flx_frontend')
-    return ocaml.Library(path / 'flx_frontend', [path / '*.ml{,i}'],
+    return ocaml.builder.build_lib(path / 'flx_frontend', [path / '*.ml{,i}'],
         libs=[
-            fbuild.env.run(build_flx_misc, builder),
-            fbuild.env.run(build_flx_core, builder),
-            fbuild.env.run(build_flx_bind, builder),
-        ],
-        builder=builder)
+            fbuild.env.run(build_flx_misc, ocaml),
+            fbuild.env.run(build_flx_core, ocaml),
+            fbuild.env.run(build_flx_bind, ocaml),
+        ])
 
-def build_flx_backend(builder):
+def build_flx_backend(ocaml):
     path = Path('src/compiler/flx_backend')
-    return ocaml.Library(path / 'flx_backend', [
+    return ocaml.builder.build_lib(path / 'flx_backend', [
             path / '*.ml{,i}',
             fbuild.buildroot / path / '*.ml{,i}',
         ],
         libs=[
             'nums',
-            fbuild.env.run(build_flx_misc, builder),
-            fbuild.env.run(build_flx_core, builder),
-            fbuild.env.run(build_flx_bind, builder),
-            fbuild.env.run(build_flxcclib, builder),
-            fbuild.env.run(build_flx_frontend, builder),
-        ],
-        builder=builder)
+            fbuild.env.run(build_flx_misc, ocaml),
+            fbuild.env.run(build_flx_core, ocaml),
+            fbuild.env.run(build_flx_bind, ocaml),
+            fbuild.env.run(build_flxcclib, ocaml),
+            fbuild.env.run(build_flx_frontend, ocaml),
+        ])
 
-def build_flx_drivers(builder):
+def build_flx_drivers(ocaml):
     path = Path('src', 'compiler', 'drivers')
 
-    lib = ocaml.Library(path / 'flx_driver', [
+    lib = ocaml.builder.build_lib(path / 'flx_driver', [
             path / 'flx_terminate.ml{,i}',
             path / 'flx_flxopt.ml{,i}',
         ],
         libs=[
-            fbuild.env.run(build_flx_misc, builder),
-            fbuild.env.run(build_flx_core, builder),
-            fbuild.env.run(build_flxcclib, builder),
-        ],
-        builder=builder)
+            fbuild.env.run(build_flx_misc, ocaml),
+            fbuild.env.run(build_flx_core, ocaml),
+            fbuild.env.run(build_flxcclib, ocaml),
+        ])
 
-    flxl = ocaml.Executable(fbuild.buildroot / 'bin/flxl', [path / 'flxl.ml'],
-        libs=[
-            lib,
-            fbuild.env.run(build_flx_misc, builder),
-            fbuild.env.run(build_flx_core, builder),
-            fbuild.env.run(build_flx_version, builder),
-            fbuild.env.run(build_flx_version_hook, builder),
-            fbuild.env.run(build_flx_lex, builder),
-            fbuild.env.run(build_flx_desugar, builder),
-        ],
-        builder=builder)
+    libs = [
+        'nums', 'unix', 'str',
+        fbuild.env.run('buildsystem.ocs.build_lib', ocaml),
+        fbuild.env.run('buildsystem.sex.build', ocaml),
+        fbuild.env.run('buildsystem.dyp.build_lib', ocaml),
+        fbuild.env.run('buildsystem.cil.build', ocaml),
+        fbuild.env.run(build_flx_misc, ocaml),
+        fbuild.env.run(build_flx_core, ocaml),
+        fbuild.env.run(build_flx_version, ocaml),
+        fbuild.env.run(build_flx_version_hook, ocaml),
+        fbuild.env.run(build_flx_lex, ocaml),
+        fbuild.env.run(build_flx_parse, ocaml),
+        fbuild.env.run(build_flxcclib, ocaml),
+        fbuild.env.run(build_flx_desugar, ocaml),
+        fbuild.env.run(build_inria_re, ocaml),
+        fbuild.env.run(build_flx_bind, ocaml),
+        fbuild.env.run(build_flx_frontend, ocaml),
+        fbuild.env.run(build_flx_backend, ocaml),
+        lib,
+    ]
 
-    flxp = ocaml.Executable(fbuild.buildroot / 'bin/flxp', [path / 'flxp.ml'],
-        libs=[
-            lib,
-            fbuild.env.run(build_flx_misc, builder),
-            fbuild.env.run(build_flx_core, builder),
-            fbuild.env.run(build_flx_version, builder),
-            fbuild.env.run(build_flx_version_hook, builder),
-            fbuild.env.run(build_flx_lex, builder),
-            fbuild.env.run(build_flx_parse, builder),
-            fbuild.env.run(build_flx_desugar, builder),
-        ],
-        builder=builder)
+    flxl = ocaml.builder.build_exe(fbuild.buildroot / 'bin/flxl',
+        [path / 'flxl.ml'], libs=libs)
 
-    flxm = ocaml.Executable(fbuild.buildroot / 'bin/flxm', [path / 'flxm.ml'],
-        libs=[
-            lib,
-            fbuild.env.run(build_flx_misc, builder),
-            fbuild.env.run(build_flx_core, builder),
-            fbuild.env.run(build_flx_version, builder),
-            fbuild.env.run(build_flx_version_hook, builder),
-            fbuild.env.run(build_flx_parse, builder),
-            fbuild.env.run(build_flx_desugar, builder),
-        ],
-        builder=builder)
+    flxp = ocaml.builder.build_exe(fbuild.buildroot / 'bin/flxp',
+        [path / 'flxp.ml'], libs=libs)
 
-    flxd = ocaml.Executable(fbuild.buildroot / 'bin/flxd', [path / 'flxd.ml'],
-        libs=[
-            lib,
-            fbuild.env.run(build_flx_misc, builder),
-            fbuild.env.run(build_flx_core, builder),
-            fbuild.env.run(build_flx_version, builder),
-            fbuild.env.run(build_flx_version_hook, builder),
-            fbuild.env.run(build_flx_desugar, builder),
-        ],
-        builder=builder)
+    flxm = ocaml.builder.build_exe(fbuild.buildroot / 'bin/flxm',
+        [path / 'flxm.ml'], libs=libs)
 
-    flxb = ocaml.Executable(fbuild.buildroot / 'bin/flxb', [path / 'flxb.ml'],
-        libs=[
-            lib,
-            fbuild.env.run(build_flx_misc, builder),
-            fbuild.env.run(build_flx_core, builder),
-            fbuild.env.run(build_flx_version, builder),
-            fbuild.env.run(build_flx_version_hook, builder),
-            fbuild.env.run(build_flx_parse, builder),
-            fbuild.env.run(build_flx_desugar, builder),
-            fbuild.env.run(build_flx_bind, builder),
-            fbuild.env.run(build_flx_frontend, builder),
-        ],
-        builder=builder)
+    flxd = ocaml.builder.build_exe(fbuild.buildroot / 'bin/flxd',
+        [path / 'flxd.ml'], libs=libs)
 
-    flxg = ocaml.Executable(fbuild.buildroot / 'bin/flxg', [path / 'flxg.ml'],
-        libs=[
-            lib,
-            fbuild.env.run(build_flx_misc, builder),
-            fbuild.env.run(build_flx_core, builder),
-            fbuild.env.run(build_flx_version, builder),
-            fbuild.env.run(build_flx_version_hook, builder),
-            fbuild.env.run(build_flxcclib, builder),
-            fbuild.env.run(build_flx_desugar, builder),
-            fbuild.env.run(build_flx_bind, builder),
-            fbuild.env.run(build_flx_frontend, builder),
-            fbuild.env.run(build_flx_backend, builder),
-        ],
-        builder=builder)
+    flxb = ocaml.builder.build_exe(fbuild.buildroot / 'bin/flxb',
+        [path / 'flxb.ml'], libs=libs)
+
+    flxg = ocaml.builder.build_exe(fbuild.buildroot / 'bin/flxg',
+        [path / 'flxg.ml'], libs=libs)
 
     return Record(
         flxl=flxl,
