379b8f3e20ff80e7fabc9d2682240ecf05944348
idadesub <idadesub@3fca3a5c-071c-0410-b500-ba939811c8f2>
2009-02-24 09:06:20 +0000

    Fix the old build system to get it compiling again.
    git-svn-id: https://felix.svn.sourceforge.net/svnroot/felix/felix/trunk@2349 3fca3a5c-071c-0410-b500-ba939811c8f2

--- spkgs/gsl.py
+++ spkgs/gsl.py
@@ -2,5 +2,4 @@ from cpkgs.target.gsl import HAVE_GSL
 if HAVE_GSL:
   pass
 
-iscr_source = ['lpsrc/gsl.pak']
 weaver_directory = 'doc/gsl/'
--- spkgs/mpi.py
+++ spkgs/mpi.py
@@ -1,4 +1,3 @@
 from cpkgs.target.mpi import HAVE_MPI
 if HAVE_MPI:
-  iscr_source = ['lpsrc/mpi.pak']
   weaver_directory = 'doc/rtl/mpi/'
