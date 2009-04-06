ba62a5d984c7393da1a9007ff639a4a57d4f9210
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-04-05 23:02:02 -0700

    Fix buildsystem.ocs to use the new submodule.

--- buildsystem/ocs.py
+++ buildsystem/ocs.py
@@ -5,14 +5,14 @@ from fbuild.record import Record
 # ------------------------------------------------------------------------------
 
 def build_lib(ocaml):
-    path = Path('src/compiler/ocs')
+    path = Path('src/compiler/ocs/src')
 
     return ocaml.build_lib(path/'ocs',
         list((path/'*.ml{,i}').glob(exclude='ocs_main.ml')),
         external_libs=['nums', 'unix'])
 
 def build_exe(ocaml):
-    path = Path('src/compiler/ocs')
+    path = Path('src/compiler/ocs/src')
 
     return ocaml.build_exe(path/'ocs', [path/'ocs_main.ml'],
         libs=[build_lib(ocaml)],
