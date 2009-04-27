2655eae9bd8bdc8d0c8b0030f5dcfb02b2de36da
John Skaller <skaller@users.sourceforge.net>
2009-04-28 03:39:47 +1000

    Remove a whole heap of lexing stuff that is no longer needed. As a side effect, interpolation strings have to be dropped from the system, since they require parsing a string during desugaring. If this is to be done at all it should be done in the parser by recursion.

--- src/compiler/flx_lex/flx_keywords.mli
+++
@@ -1,4 +0,0 @@
-open Flx_token
-val flx_keywords : (string * string) list
-val flx_syms : (string * string) list
-val map_flx_keywords : Flx_ast.srcref -> string -> token
--- src/compiler/flx_lex/flx_lex.mli
+++
@@ -1,9 +0,0 @@
-val pre_flx_lex :
-  Flx_lexstate.lexer_state ->
-  Lexing.lexbuf ->
-  Flx_token.token list
-
-val parse_line :
-  Flx_lexstate.lexer_state ->
-  Lexing.lexbuf ->
-  string
