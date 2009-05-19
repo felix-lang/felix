99d3547057f42d84eeb8aaed7a9104421c7c3e31
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-05-19 01:19:02 -0700

    Clean up types flx_bexe.mli.

--- src/compiler/flx_bind/flx_bexe.mli
+++ src/compiler/flx_bind/flx_bexe.mli
@@ -1,16 +1,12 @@
 (** Bind executable statements *)
 
-open Flx_ast
-open Flx_types
-open Flx_mtypes2
-
 val bind_exes:
-  sym_state_t ->
-  env_t ->
+  Flx_mtypes2.sym_state_t ->
+  Flx_types.env_t ->
   Flx_srcref.t ->
-  (Flx_srcref.t * exe_t) list ->
-  btypecode_t ->
+  (Flx_srcref.t * Flx_ast.exe_t) list ->
+  Flx_types.btypecode_t ->
   string ->
-  bid_t ->
-  bvs_t ->
-  btypecode_t * bexe_t list
+  Flx_ast.bid_t ->
+  Flx_types.bvs_t ->
+  Flx_types.btypecode_t * Flx_types.bexe_t list
