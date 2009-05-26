eae81df75db02cd3b439a28349b7f16480205648
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-05-25 23:04:19 -0700

    Explicitly reference modules in Flx_desugar.

--- src/compiler/flx_desugar/flx_desugar.mli
+++ src/compiler/flx_desugar/flx_desugar.mli
@@ -1,8 +1,5 @@
-open Flx_ast
-open Flx_types
-open Flx_mtypes2
 val desugar_program:
-  sym_state_t ->
+  Flx_mtypes2.sym_state_t ->
   string ->
-  statement_t list ->
-  asm_t list
+  Flx_ast.statement_t list ->
+  Flx_types.asm_t list
