4067ef8efa3aec98c6cc69033741b856fbdb8b67
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-06-18 01:21:03 -0700

    Get takfp working for c, felix, and ocaml.

--- speed/takfp/c/takfp.c
+++ speed/takfp/c/takfp.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
+#include <time.h>
 
 float Tak (float x, float y, float z)
 {
     if (y >= x) return z;
-    return Tak(Tak(x-1,y,z), Tak(y-1,z,x), Tak(z-1,x,y));
+    return Tak(Tak(x-1.0f,y,z), Tak(y-1.0f,z,x), Tak(z-1.0f,x,y));
 }
 
 int main(int argc, char* argv[])
 {
-    /* int n = ((argc == 2) ? atoi(argv[1]) : 1); */
-    clock_t start, end;
-    double cpu_time_used;
     int n = 10;
-    start = clock();
+    clock_t t0 = clock();
     double r = Tak(n*3.0, n*2.0, n*1.0);
-    //printf("%.1f\n", Tak(n*3.0, n*2.0, n*1.0));
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    printf("%f\n",cpu_time_used);
+    clock_t t1 = clock();
+    printf("%.2f\n", r);
+    printf("%f\n", ((double) (t1 - t0)) / CLOCKS_PER_SEC);
     return 0;
 }
 
--- speed/takfp/ocaml/takfp.ml
+++ speed/takfp/ocaml/takfp.ml
@@ -3,7 +3,9 @@ let rec tak x y z =
   else tak (tak (x -. 1.) y z) (tak (y -. 1.) z x) (tak (z -. 1.) x y)
 
 let () =
-  let n = float_of_string(Array.get Sys.argv 1) in
-  Printf.printf "%.1f\n" (tak (3. *. n) (2. *. n) n)
-;;
-
+  let n = 10. in
+  let t0 = Unix.gettimeofday () in
+  let v = (tak (3. *. n) (2. *. n) n) in
+  let t1 = Unix.gettimeofday () in
+  Printf.printf "%.2f\n" v;
+  Printf.printf "%f\n" (t1 -. t0)
