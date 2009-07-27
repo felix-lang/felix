1ecc4ebb0ac659c17376dec08097eae272d0bb02
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-07-27 01:16:20 -0700

    Add an interative form of strabs.

--- src/compiler/flx_bind/flx_strabs.mli
+++ src/compiler/flx_bind/flx_strabs.mli
@@ -10,6 +10,12 @@ val make_strabs_state:
   Flx_types.fully_bound_symbol_table_t -> (** The output symbol table *)
   strabs_state_t
 
+val strabs_symbol:
+  strabs_state_t -> (** The state needed for downgrading types. *)
+  int ->
+  Flx_types.symbol_data3_t ->
+  Flx_types.symbol_data3_t option
+
 val strabs:
-  strabs_state_t -> (** The state needed *)
+  strabs_state_t -> (** The state needed for downgrading types. *)
   unit
