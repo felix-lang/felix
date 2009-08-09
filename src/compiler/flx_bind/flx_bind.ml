5e35a0c5a219865fba0b9342523d52912c264404
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-08-09 10:54:33 -0700

    Rename variable to be easier to read.

--- src/compiler/flx_bind/flx_bind.ml
+++ src/compiler/flx_bind/flx_bind.ml
@@ -19,42 +19,42 @@ let make_bind_state syms =
     strabs_state = Flx_strabs.make_strabs_state bbind_bbdfns strabs_bbdfns;
   }
 
-let bind_asm ?parent bind_state handle_symbol asm init =
+let bind_asm ?parent state handle_symbol asm init =
   (* We need to save the symbol index counter so we can bind all of the symbols
    * that we just added. *)
-  let i = ref !(bind_state.syms.Flx_mtypes2.counter) in
+  let i = ref !(state.syms.Flx_mtypes2.counter) in
 
   (* Add declarations to the symbol table *)
   begin
     match asm with
     | Flx_types.Exe exe -> ()
     | Flx_types.Dcl dcl ->
-        ignore(Flx_symtab.add_dcl ?parent bind_state.symtab dcl)
+        ignore(Flx_symtab.add_dcl ?parent state.symtab dcl)
     | Flx_types.Iface (sr,iface) ->
         let biface = Flx_bbind.bind_interface
-          bind_state.bbind_state
-          (sr,iface,parent)
+          state.bbind_state
+          (sr, iface, parent)
         in
-        bind_state.syms.Flx_mtypes2.bifaces <-
-          biface :: bind_state.syms.Flx_mtypes2.bifaces
+        state.syms.Flx_mtypes2.bifaces <-
+          biface :: state.syms.Flx_mtypes2.bifaces
     | Flx_types.Dir dir -> ()
   end;
 
   (* Now bind in order all of the symbols we added. *)
   let init = ref init in
 
-  while !i < !(bind_state.syms.Flx_mtypes2.counter) do
+  while !i < !(state.syms.Flx_mtypes2.counter) do
     (* First, find the symbol to bind. *)
     begin
-      match Flx_hashtbl.find bind_state.syms.Flx_mtypes2.dfns !i with
+      match Flx_hashtbl.find state.syms.Flx_mtypes2.dfns !i with
       | None -> ()
       | Some s ->
           (* Then, bind the symbol. *)
-          match Flx_bbind.bbind_symbol bind_state.bbind_state !i s with
+          match Flx_bbind.bbind_symbol state.bbind_state !i s with
           | None -> ()
           | Some s ->
               (* Finally, downgrade abstract types. *)
-              match Flx_strabs.strabs_symbol bind_state.strabs_state !i s with
+              match Flx_strabs.strabs_symbol state.strabs_state !i s with
               | None -> ()
               | Some s ->
                   init := handle_symbol !i s !init
@@ -64,22 +64,22 @@ let bind_asm ?parent bind_state handle_symbol asm init =
 
   !init
 
-let bind_asms bind_state asms =
+let bind_asms state asms =
   (* Add the symbols to the symtab. *)
-  let exes, ifaces = Flx_symtab.add_asms bind_state.symtab asms in
+  let exes, ifaces = Flx_symtab.add_asms state.symtab asms in
 
   (* Now, bind all the symbols. *)
-  Flx_bbind.bbind bind_state.bbind_state;
+  Flx_bbind.bbind state.bbind_state;
 
   (* Downgrade abstract types. *)
-  Flx_strabs.strabs bind_state.strabs_state;
+  Flx_strabs.strabs state.strabs_state;
 
   (* Bind the interfaces. *)
-  bind_state.syms.Flx_mtypes2.bifaces <- List.map
-    (Flx_bbind.bind_interface bind_state.bbind_state) ifaces;
+  state.syms.Flx_mtypes2.bifaces <- List.map
+    (Flx_bbind.bind_interface state.bbind_state) ifaces;
 
   (* Clear the type cache. *)
-  Hashtbl.clear bind_state.syms.Flx_mtypes2.ticache;
+  Hashtbl.clear state.syms.Flx_mtypes2.ticache;
 
   (* Return the bound symbol table. *)
-  bind_state.strabs_bbdfns
+  state.strabs_bbdfns
