7cbfa770ad226b87abb6ac59952fe5c3b454a5b2
Erick Tryzelaar <idadesub@users.sourceforge.net>
2009-10-05 09:12:26 -0700

    Make sdl-1.3 optional.

--- buildsystem/bindings.py
+++ buildsystem/bindings.py
@@ -75,15 +75,13 @@ def build_flx(phase):
     dsts.extend(buildsystem.copy_to(phase.ctx,
         phase.ctx.buildroot / 'lib/GL', Path('src/opengl/*.flx').glob()))
 
-    if fbuild.config.c.sdl.SDL_SDL_h(phase.cxx.shared).header:
-        sdl_config = fbuild.builders.sdl.SDLConfig(phase.ctx,
-                requires_at_least_version=(1, 3))
+    if phase.sdl_config:
         sdl_fpc = fbuild.builders.text.autoconf_config_file(phase.ctx,
             'src/sdl/sdl.fpc', 'src/sdl/sdl.fpc.in', {
-                'VERSION': sdl_config.version(),
-                'CFLAGS': sdl_config.cflags(),
-                'SHARED_LIBS': sdl_config.libs(),
-                'STATIC_LIBS': sdl_config.static_libs(),
+                'VERSION': phase.sdl_config.version(),
+                'CFLAGS': phase.sdl_config.cflags(),
+                'SHARED_LIBS': phase.sdl_config.libs(),
+                'STATIC_LIBS': phase.sdl_config.static_libs(),
             })
 
         dsts.extend(buildsystem.copy_fpc_to_config(phase.ctx, [sdl_fpc]))
