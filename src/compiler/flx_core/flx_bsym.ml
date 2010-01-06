abee5282460084efd0e50d6225163c0b6f1dff00
Erick Tryzelaar <idadesub@users.sourceforge.net>
2010-01-06 11:26:45 -0800

    Look up in Flx_types.bvs_of_bbdcl instead of computing it.

--- src/compiler/flx_core/flx_bsym.ml
+++ src/compiler/flx_core/flx_bsym.ml
@@ -45,23 +45,4 @@ let is_generator bsym =
 
 (** Returns the bound type value list of the bound symbol. *)
 let get_bvs bsym =
-  match bsym.bbdcl with
-  | BBDCL_function (props,bvs,(ps,traint),ret,exes) -> bvs
-  | BBDCL_procedure (props,bvs,(ps,traint),exes) -> bvs
-  | BBDCL_val (bvs,t) -> bvs
-  | BBDCL_var (bvs,t) -> bvs
-  | BBDCL_ref (bvs,t) -> bvs
-  | BBDCL_tmp (bvs,t) -> bvs
-  | BBDCL_union (bvs,ps) -> bvs
-  | BBDCL_struct (bvs,ps) -> bvs
-  | BBDCL_cstruct (bvs,ps) -> bvs
-  | BBDCL_newtype (bvs,t) -> bvs
-  | BBDCL_const (_,bvs,t,ct,reqs) -> bvs
-  | BBDCL_insert (bvs,s,ikind,reqs) -> bvs
-  | BBDCL_fun (props,bvs,argtypes,ret,ct,reqs,prec) -> bvs
-  | BBDCL_callback (props,bvs,argtypes_cf,argtypes_c,k,ret,reqs,prec) -> bvs
-  | BBDCL_proc (props,bvs,argtypes,ct,reqs) -> bvs
-  | BBDCL_abs (bvs,tqual,ct,reqs) -> bvs
-  | BBDCL_nonconst_ctor (bvs,uidx,udt, ctor_idx, ctor_argt, ebvs, etraint) -> bvs
-  | BBDCL_typeclass (props,bvs) -> bvs
-  | BBDCL_instance (props,bvs,con,tc,ts) -> bvs
+  Flx_types.bvs_of_bbdcl bsym.bbdcl
