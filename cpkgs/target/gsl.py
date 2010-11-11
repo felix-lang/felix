15cd6ccd4196e332d925c61446d14958876066a5
skaller <Max.Skaller@gmail.com>
2010-11-11 14:07:38 +1100

    Get rid of the cpkgs, seems they're not used.

--- cpkgs/target/gsl.py
+++
@@ -1,9 +0,0 @@
-try:
-  from config.target_gsl_config import *
-except ImportError:
-  import config
-  from fbuild.flxbuild.config_support import cwrite, pa
-  HAVE_GSL = config.TARGET_CXX.check_header_exists('gsl/gsl_blas.h')
-  f = cwrite('target_gsl')
-  pa(f, locals(), "HAVE_GSL")
-  f.close()
--- cpkgs/target/pari.py
+++
@@ -1,9 +0,0 @@
-try:
-  from config.target_pari_config import *
-except ImportError:
-  import config
-  from fbuild.flxbuild.config_support import cwrite, pa
-  HAVE_PARI = config.TARGET_CXX.check_header_exists('pari.h')
-  f = cwrite('target_pari')
-  pa(f, locals(), "HAVE_PARI")
-  f.close()
