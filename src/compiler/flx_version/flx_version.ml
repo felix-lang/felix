b1dff93c0de29e245431dd46c0f9761841355ea9
skaller <Max.Skaller@gmail.com>
2010-12-09 17:46:05 +1100

    Change version record in Ocaml to omit the buildno, since it is useless.

--- src/compiler/flx_version/flx_version.ml
+++ src/compiler/flx_version/flx_version.ml
@@ -3,7 +3,6 @@ type version_data_t =
   version_string : string;
   build_time_float : float;
   build_time : string;
-  buildno : int;
 }
 
 let version_data = ref
@@ -11,7 +10,6 @@ let version_data = ref
   version_string = "no version";
   build_time_float = -1.0;
   build_time = "0000-00-00";
-  buildno = -1;
 }
 
 
--- src/compiler/flx_version/flx_version.mli
+++ src/compiler/flx_version/flx_version.mli
@@ -3,7 +3,6 @@ type version_data_t =
   version_string : string;
   build_time_float : float;
   build_time : string;
-  buildno : int;
 }
 
 val version_data: version_data_t ref
