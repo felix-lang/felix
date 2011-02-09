bba3e8fc9d5d8969815e82f3537de068900c72e6
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2011-02-09 10:31:39 -0800

    Only compile flx_cp on osx for now.
    I'm disabling this for the other Os-es because the
    filesystem open flags like O_SHLOCK is not implemented
    on linux.

--- buildsystem/tools.py
+++ buildsystem/tools.py
@@ -1,3 +1,4 @@
+import fbuild
 import fbuild.builders.file
 
 # ------------------------------------------------------------------------------
@@ -6,8 +7,9 @@ def build(phase, felix):
     exe = felix.compile('tools/flx_ls.flx', static=True)
     fbuild.builders.file.copy(phase.ctx, exe, 'bin')
 
-    exe = felix.compile('tools/flx_cp.flx', static=True)
-    fbuild.builders.file.copy(phase.ctx, exe, 'bin')
+    if 'macosx' in phase.platform:
+        exe = felix.compile('tools/flx_cp.flx', static=True)
+        fbuild.builders.file.copy(phase.ctx, exe, 'bin')
 
     exe = felix.compile('tools/webserver.flx', static=True)
     fbuild.builders.file.copy(phase.ctx, exe, 'bin')
