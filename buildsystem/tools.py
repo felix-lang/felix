ce3c8bcbe2dd05156a4e769c4937a5172fef5295
skaller <Max.Skaller@gmail.com>
2011-02-13 00:18:02 +1100

    Get rid of the osx only flags in flx_cp so it builds on linux.

--- buildsystem/tools.py
+++ buildsystem/tools.py
@@ -7,9 +7,8 @@ def build(phase, felix):
     exe = felix.compile('tools/flx_ls.flx', static=True)
     fbuild.builders.file.copy(phase.ctx, exe, 'bin')
 
-    if 'macosx' in phase.platform:
-        exe = felix.compile('tools/flx_cp.flx', static=True)
-        fbuild.builders.file.copy(phase.ctx, exe, 'bin')
+    exe = felix.compile('tools/flx_cp.flx', static=True)
+    fbuild.builders.file.copy(phase.ctx, exe, 'bin')
 
     exe = felix.compile('tools/webserver.flx', static=True)
     fbuild.builders.file.copy(phase.ctx, exe, 'bin')
