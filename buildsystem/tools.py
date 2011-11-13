da1603acafe36a4c531b2be85516c5bad0f859ad
skaller <Max.Skaller@gmail.com>
2011-11-13 14:04:09 +1000

    Fix build bug re new location of tools.

--- buildsystem/tools.py
+++ buildsystem/tools.py
@@ -4,11 +4,11 @@ import fbuild.builders.file
 # ------------------------------------------------------------------------------
 
 def build(phase, felix):
-    exe = felix.compile('tools/flx_ls.flx', static=True)
+    exe = felix.compile('src/tools/flx_ls.flx', static=True)
     fbuild.builders.file.copy(phase.ctx, exe, 'bin')
 
-    exe = felix.compile('tools/flx_cp.flx', static=True)
+    exe = felix.compile('src/tools/flx_cp.flx', static=True)
     fbuild.builders.file.copy(phase.ctx, exe, 'bin')
 
-    exe = felix.compile('tools/webserver.flx', static=True)
+    exe = felix.compile('src/tools/webserver.flx', static=True)
     fbuild.builders.file.copy(phase.ctx, exe, 'bin')
