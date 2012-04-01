b77596ac8b73f0c00d613772059494ca5c36e946
skaller <Max.Skaller@gmail.com>
2012-04-02 08:41:15 +1000

    Fix install so webserver works.
    We put the plugins in /usr/local/lib on the assumption
    the loader path will include that. The build system
    is adjusted to make the plugins now. Note also it makes
    build/release/shlib for the plugins. However the install
    actually sources the binaries for the webserver from
    build/release/tools. YMMV :)

--- buildsystem/tools.py
+++ buildsystem/tools.py
@@ -1,6 +1,6 @@
 import fbuild
 import fbuild.builders.file
-
+import os
 # ------------------------------------------------------------------------------
 
 def build(phase, felix):
@@ -12,6 +12,29 @@ def build(phase, felix):
 
     exe = felix.compile(phase.ctx.buildroot/'tools/webserver.flx', static=True)
     fbuild.builders.file.copy(phase.ctx, exe, 'bin')
+
+    try:
+      os.mkdir(phase.ctx.buildroot/'shlib')
+    except:
+      pass
+
+    shlib = felix.compile(phase.ctx.buildroot/'tools/ocaml2html.flx')
+    fbuild.builders.file.copy(phase.ctx, shlib, 'shlib')
+
+    shlib = felix.compile(phase.ctx.buildroot/'tools/py2html.flx')
+    fbuild.builders.file.copy(phase.ctx, shlib, 'shlib')
+
+    shlib = felix.compile(phase.ctx.buildroot/'tools/fdoc2html.flx')
+    fbuild.builders.file.copy(phase.ctx, shlib, 'shlib')
+
+    shlib = felix.compile(phase.ctx.buildroot/'tools/flx2html.flx')
+    fbuild.builders.file.copy(phase.ctx, shlib, 'shlib')
+
+    shlib = felix.compile(phase.ctx.buildroot/'tools/cpp2html.flx')
+    fbuild.builders.file.copy(phase.ctx, shlib, 'shlib')
+
+    shlib = felix.compile(phase.ctx.buildroot/'tools/fpc2html.flx')
+    fbuild.builders.file.copy(phase.ctx, shlib, 'shlib')
 
     exe = felix.compile(phase.ctx.buildroot/'tools/norK.flx', static=True)
     fbuild.builders.file.copy(phase.ctx, exe, 'bin')
