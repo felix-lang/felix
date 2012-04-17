f7ab1cc0a254b00d9a083459dda05d2e760ffeb1
skaller <Max.Skaller@gmail.com>
2012-04-17 20:36:33 +1000

    And the interface..

--- src/compiler/flx_opt/flx_opt.mli
+++ src/compiler/flx_opt/flx_opt.mli
@@ -5,12 +5,3 @@ val optimize_bsym_table :
   Flx_types.bid_t ->          (** The root procedure. *)
   Flx_bsym_table.t
 
-(** Perform high level optimizations across the given set of bound exes and
- * symbols.*)
-val optimize :
-  Flx_mtypes2.sym_state_t ->  (** The felix state. *)
-  Flx_bsym_table.t ->         (** The bound symbol table. *)
-  Flx_types.bid_t ->          (** The root procedure. *)
-  Flx_types.bid_t list ->     (** The symbols to optimize. *)
-  Flx_bexe.t list ->          (** The symbols to optimize. *)
-  Flx_bsym_table.t * Flx_types.bid_t list * Flx_bexe.t list
