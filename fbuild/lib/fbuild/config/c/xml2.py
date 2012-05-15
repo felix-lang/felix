a639e1b6acd40da03240273a7e7caec261c4af35
skaller <Max.Skaller@gmail.com>
2012-05-16 08:46:37 +1000

    Get rid of need for stddef.h and stdint.h in config.
    Make config non-portable. But removes obligation for
    stdint.h to exist and define the required types.
    Lame excuse for Windows and other stupid platforms
    that use the licence to not define int types.

---
+++ fbuild/lib/fbuild/config/c/xml2.py
@@ -0,0 +1,7 @@
+
+import fbuild.config.c as c
+
+# ------------------------------------------------------------------------------
+
+class libxml2_libxml_parser_h(c.Test):
+    header = c.header_test('libxml2/libxml/parser.h')
