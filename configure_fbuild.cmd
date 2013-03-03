9c70c391508501380d7bde9af2e4d830077b209f
Shayne Fletcher <shayne.fletcher.50@gmail.com>
2013-03-02 22:27:07 -0500

    Just extern, not extern C on static_create_thread_frame and friend

--- configure_fbuild.cmd
+++ configure_fbuild.cmd
@@ -18,23 +18,20 @@ set "SYSTEM_PATH=C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem"
 set "PYTHON_PATH=C:\Python32"
 set "TCL_PATH=C:\Tcl\bin"
 set "FLEXDLL_PATH=%PFPATH%\flexdll"
-set "OCAML_PATH=C:\ocamlms\ocaml-4.00.1\bin"
-set "OCAMLLIB=C:\ocamlms\ocaml-4.00.1\lib"
-
-echo   * SYSTEM_PATH=%SYSTEM_PATH%
-echo   * PYTHON_PATH=%PYTHON_PATH%
-echo   * TCL=%TCL_PATH%
-echo   * FLEXDLL=%FLEXDLL_PATH%
-echo   * OCAML_PATH=%OCAML_PATH%
-echo   * OCAMLLIB=%OCAMLLIB%
-
+set "OCAML_PATH=C:\ocamlms\bin"
+set "OCAMLLIB=C:\ocamlms\lib"
 set "PATH=%PYTHON_PATH%;%TCL_PATH%;%FLEXDLL_PATH%;%OCAML_PATH%;%SYSTEM_PATH%"
 
-echo   * PATH=%PATH%
+echo * SYSTEM_PATH=%SYSTEM_PATH%
+echo * PYTHON_PATH=%PYTHON_PATH%
+echo * TCL=%TCL_PATH%
+echo * FLEXDLL=%FLEXDLL_PATH%
+echo * OCAML_PATH=%OCAML_PATH%
+echo * OCAMLLIB=%OCAMLLIB%
+echo * PATH=%PATH%
 
 :: Pull in Microsoft toolset.
 
 call "%PFPATH%\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat" > nul 2>&1
   
 echo Good hunting!
-
