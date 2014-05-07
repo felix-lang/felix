5860985aa767b1181a1ad87c6968c09435468ea0
skaller <Max.Skaller@gmail.com>
2014-05-08 09:21:26 +1000

    Fix serialisation bug, bump version number 1.1.12.

--- buildsystem/version.py
+++ buildsystem/version.py
@@ -1,5 +1,5 @@
 flx_version_major = '1'
 flx_version_minor = '1'
-flx_version_patch = '11'
+flx_version_patch = '12'
 flx_version_release = ''
 flx_version = flx_version_major+'.'+flx_version_minor+'.'+flx_version_patch+flx_version_release
