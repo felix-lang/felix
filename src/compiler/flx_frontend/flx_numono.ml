b510ccb5894131c1d27f16af7741ef986831bb6b
skaller <Max.Skaller@gmail.com>
2014-09-22 11:29:12 +1000

    Add analysis routine fromold mono.

--- src/compiler/flx_frontend/flx_numono.ml
+++ src/compiler/flx_frontend/flx_numono.ml
@@ -6,6 +6,9 @@ open Flx_mtypes2
 open Flx_print
 open Flx_types
 open Flx_bbdcl
+open Flx_bexpr
+open Flx_bexe
+open Flx_bparameter
 
 type symkind = Felix of int | External
 
@@ -33,6 +36,150 @@ let find_felix_inst syms processed to_process i ts : int =
   | Felix k -> k
   | _ -> assert false
  
+
+let fixup_type' syms bsym_table fi t =
+  match t with
+  | BTYP_inst (i,ts) ->
+    (* typeclass fixup *)
+    let i,ts = fi i ts in
+    let t = btyp_inst (i,ts) in
+    t
+  | x -> x
+
+let rec fixup_type syms bsym_table fi t =
+  let f_btype t = fixup_type syms bsym_table fi t in
+  let f_btype' t = fixup_type' syms bsym_table fi t in
+  let t = Flx_btype.map ~f_btype t in
+  f_btype' t
+
+let fixup_expr' syms bsym_table fi mt (e,t) =
+  (*
+  print_endline ("FIXUP EXPR(up) " ^ sbe sym_table (e, btyp_void ()));
+  *)
+  let x = match e with
+  | BEXPR_apply_prim (i',ts,a) ->
+    let i,ts = fi i' ts in
+    if i = i' then
+      bexpr_apply_prim t (i,ts,a)
+    else
+      bexpr_apply_direct t (i,ts,a)
+
+  | BEXPR_apply_direct (i,ts,a) ->
+    let i,ts = fi i ts in
+    bexpr_apply_direct t (i,ts,a)
+
+  | BEXPR_apply_struct (i,ts,a) ->
+    let i,ts = fi i ts in
+    bexpr_apply_struct t (i,ts,a)
+
+  | BEXPR_apply_stack (i,ts,a) ->
+    let i,ts = fi i ts in
+    bexpr_apply_stack t (i,ts,a)
+
+  | BEXPR_ref (i,ts)  ->
+    let i,ts = fi i ts in
+    bexpr_ref t (i,ts)
+
+  | BEXPR_name (i',ts') ->
+    let i,ts = fi i' ts' in
+    (*
+    print_endline (
+      "Ref to Variable " ^ si i' ^ "[" ^ catmap "," (sbt bsym_table) ts' ^"]" ^
+      " mapped to " ^ si i ^ "[" ^ catmap "," (sbt bsym_table) ts ^"]"
+    );
+    *)
+    bexpr_name t (i,ts)
+
+  | BEXPR_closure (i,ts) ->
+    let i,ts = fi i ts in
+    bexpr_closure t (i,ts)
+
+  | x -> x, t
+  in
+  (*
+  print_endline ("FIXed UP EXPR " ^ sbe sym_table (x, btyp_void ()));
+  *)
+  x
+
+let rec fixup_expr syms bsym_table fi mt e =
+  (*
+  print_endline ("FIXUP EXPR(down) " ^ sbe sym_table e);
+  *)
+  let f_bexpr e = fixup_expr syms bsym_table fi mt e in
+  let f_bexpr' e = fixup_expr' syms bsym_table fi mt e in
+  let e = Flx_bexpr.map ~f_btype:mt ~f_bexpr e in
+  f_bexpr' e
+
+let fixup_exe syms bsym_table fi mt exe =
+  (*
+  print_endline ("FIXUP EXE[In] =" ^ string_of_bexe sym_table 0 exe);
+  *)
+  let f_bexpr e = fixup_expr syms bsym_table fi mt e in
+  let result =
+  match Flx_bexe.map ~f_btype:mt ~f_bexpr exe with
+  | BEXE_call_direct (sr, i,ts,a) -> assert false
+    (*
+    let i,ts = fi i ts in
+    bexe_call_direct (sr,i,ts,a)
+    *)
+
+  | BEXE_jump_direct (sr, i,ts,a) -> assert false
+    (*
+    let i,ts = fi i ts in
+    bexe_jump_direct (sr,i,ts,a)
+    *)
+
+  | BEXE_call_prim (sr, i',ts,a) -> assert false
+    (*
+    let i,ts = fi i' ts in
+    if i = i' then
+      bexe_call_prim (sr,i,ts,a)
+    else
+      bexe_call_direct (sr,i,ts,a)
+    *)
+
+  | BEXE_call_stack (sr, i,ts,a) -> assert false
+    (*
+    let i,ts = fi i ts in
+    bexe_call_stack (sr,i,ts,a)
+    *)
+
+  (* this is deviant case: implied ts is vs of parent! *)
+  | BEXE_init (sr,i,e) ->
+    (*
+    print_endline ("[init] Deviant case variable " ^ si i);
+    *)
+    let vs = Flx_bsym_table.find_bvs bsym_table i in
+    let ts = List.map (fun (s,j) -> mt (btyp_type_var (j, btyp_type 0))) vs in
+    let i,ts = fi i ts in
+    (*
+    print_endline ("[init] Remapped deviant variable to " ^ si i);
+    *)
+    bexe_init (sr,i,e)
+
+  | BEXE_svc (sr,i) ->
+    (*
+    print_endline ("[svc] Deviant case variable " ^ si i);
+    *)
+    let vs = Flx_bsym_table.find_bvs bsym_table i in
+    let ts = List.map (fun (s,j) -> mt (btyp_type_var (j, btyp_type 0))) vs in
+    let i,ts = fi i ts in
+    (*
+    print_endline ("[svc] Remapped deviant variable to " ^ si i);
+    *)
+    bexe_svc (sr,i)
+
+  | x -> x
+  in
+  (*
+  print_endline ("FIXUP EXE[Out]=" ^ string_of_bexe sym_table 0 result);
+  *)
+  result
+
+
+let fixup_exes syms bsym_table fi mt exes =
+  List.map (fixup_exe syms bsym_table fi mt) exes
+
 let rec mono_element debug syms to_process processed bsym_table nutab i ts j =
   print_endline ("numono: " ^ si i ^ "[" ^ catmap "," (sbt bsym_table) ts ^"] -> " ^ si j);
   if i = j then begin
