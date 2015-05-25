05c190c5b3d08fed29f3a316475cb8dcb78fbbdf
Ryan Gonzalez <rymg19@gmail.com>
2015-05-25 18:43:43 -0500

    Add version of flx_iscr in Python

---
+++ src/tools/flx_iscr.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# Core interscript interpreter
+import sys, os, re, io, operator, shutil, string
+
+# The regexes that the processor uses.
+class regexes:
+    felt = '[A-Za-z._][A-Za-z0-9_.]*'
+    fname = '(?:%s/)*%s' % (felt, felt)
+
+    # A tangler definition looks like:
+    # @tangler <name> = <filename>
+    tangler_def = re.compile('@tangler\s*(%s)\s*=\s*(%s)' % (felt, fname))
+    # To set the output we just use:
+    # @tangle <name>
+    tangler_use = re.compile('@tangle\s*(%s)' % felt)
+
+class State: pass
+class Doc(State): pass
+class Tangling(State): pass
+
+# Now define a type to hold a tangler file buffer.
+class Tangler(io.StringIO):
+    def __init__(self, filename, id):
+        self.filename = filename
+        self.id = id
+        super(Tangler, self).__init__()
+    # Now for the save routine. We only write the buffer to
+    # the file if the contents differ from the old file.
+    # This is to preserve the time stamp if things don't change.
+    # Note if a file is non-existent, and the tangler input
+    # is empty, the file will not be created.
+    def save(self):
+        data = self.getvalue().split('\n')
+        update = False
+        try:
+            with open(self.filename) as old:
+                update = all(map(operator.eq, old, data))
+        except:
+            update = True
+        if update:
+            print('Write     %s -> %s' % (self.id, self.filename))
+            os.makedirs(os.path.dirname(self.filename), exist_ok=True)
+            try:
+                f = open(self.filename, 'w')
+            except IOError as ex:
+                sys.exit("Can't open output file %s: %s" % (self.filename, ex))
+            with f:
+                self.seek(0)
+                shutil.copyfileobj(self, f)
+        else:
+            print('Unchanged %s -> %s' % (self.id, self.filename))
+
+# The processor class.
+class Processor:
+    def __init__(self, odir):
+        self.state = Doc
+        self.odir = odir
+        self.tangler = None
+        # We use a dictionary variable to hold all the tanglers.
+        self.tanglers = {}
+        self.lineno = 0
+    def parse_error(self, msg):
+        sys.exit('error at line %d: %s' % (self.lineno, msg))
+    # Now define the parser actions.
+    def def_tangler(self, id, filename):
+        'Add a new tangler.'
+        filename = string.Template(filename).safe_substitute(os.environ)
+        if id in self.tanglers:
+            sys.exit('Duplicate definition of tangler %s' % id)
+        self.tanglers[id] = Tangler(filename, id)
+    def set_tangler(self, id):
+        'Specify a new current tangler.'
+        try:
+            tangler = self.tanglers[id]
+        except KeyError:
+            sys.exit("Can't find tangler %s" % id)
+        else:
+            self.state = Tangling
+            self.tangler = tangler
+    def process(self, f):
+        '''
+        Here's the main processing routine for the input file.
+        We check for an @ character at the start of a line.
+        If we don't find one we either write the line to the
+        current tangler or just skip over it. If we do we have
+        to see what command it is: either a command to define
+        a new tangler, a command to switch to a different output
+        file, or a switch to document mode in which we just skip
+        over the lines.
+        '''
+        for i, line in enumerate(f, start=1):
+            self.lineno = i
+            line = line.strip()
+            if not line: continue
+            if line[0] == '@':
+                if line.startswith('@tangler'):
+                    m = regexes.tangler_def.match(line)
+                    if not m:
+                        self.parse_error('invalid tangler definition')
+                    self.def_tangler(*m.groups())
+                elif line.startswith('@tangle'):
+                    m = regexes.tangler_use.match(line)
+                    if not m:
+                        self.parse_error('invalid tangler usage')
+                    self.set_tangler(m.group(1))
+                else:
+                    self.state = Doc
+            else:
+                if self.state is Tangling:
+                    assert self.tangler
+                    print(line, file=self.tangler)
+    def save(self):
+        for tangler in self.tanglers.values():
+            tangler.save()
+            tangler.close()
+
+def iscr():
+    # Parse the arguments.
+    try:
+        _, iname, odir = sys.argv+([''] if len(sys.argv) == 2 else [])
+    except ValueError:
+        sys.exit('invalid number of arguments')
+    # If odir == '', abspath returns the current directory.
+    odir = os.path.abspath(odir)
+    p = Processor(odir)
+    # Process the input file and buffer up the code.
+    try:
+        f = open(iname)
+    except IOError as ex:
+        sys.exit(str(ex))
+    # Finally just dump the buffers to the associated
+    # files if the contents of the buffer and file differ.
+    # Do nothing if the contents are the same to avoid
+    # spoiling the last modification timestamp.
+    with f:
+        p.process(f)
+    p.save()
+
+if __name__ == '__main__':
+    iscr()
