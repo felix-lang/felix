3864494da256588545914466e9aca32041545051
skaller <skaller@users.sourceforge.net>
2015-07-19 11:52:06 +1000

    correct C++ quals ::flx::rtl::cl_t

--- src/rtl/flx_rtl_shapes.hpp
+++ src/rtl/flx_rtl_shapes.hpp
@@ -18,6 +18,7 @@ RTL_EXTERN extern ::flx::gc::generic::gc_shape_t _fthread_ptr_map;
 RTL_EXTERN extern ::flx::gc::generic::gc_shape_t schannel_ptr_map;
 RTL_EXTERN extern ::flx::gc::generic::gc_shape_t _uctor_ptr_map;
 RTL_EXTERN extern ::flx::gc::generic::gc_shape_t _int_ptr_map;
+RTL_EXTERN extern ::flx::gc::generic::gc_shape_t cl_t_ptr_map;
 RTL_EXTERN extern ::flx::gc::generic::gc_shape_t _address_ptr_map;
 //RTL_EXTERN extern ::flx::gc::generic::gc_shape_t _caddress_ptr_map;
 RTL_EXTERN extern ::flx::gc::generic::gc_shape_t slist_node_ptr_map;
