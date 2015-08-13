43528e0397bf2bfa89f539b1fbb14a1e273f6f2b
John Skaller <skaller@users.sourceforge.net>
2015-08-13 21:25:55 +1000

    Packaging ..

--- buildsystem/flx_stdlib.py
+++ buildsystem/flx_stdlib.py
@@ -8,20 +8,20 @@ def build_flx(phase):
     path = Path(phase.ctx.buildroot/'share'/'src/lib')
 
     dsts = []
-    dsts.extend(buildsystem.copy_flxs_to_lib(phase.ctx,
-        (path / '*.flx{,h}').glob()))
-    dsts.extend(buildsystem.copy_flxs_to_lib(phase.ctx,
-        (path / '*.fdoc').glob()))
+    #dsts.extend(buildsystem.copy_flxs_to_lib(phase.ctx,
+    #    (path / '*.flx{,h}').glob()))
+    #dsts.extend(buildsystem.copy_flxs_to_lib(phase.ctx,
+    #    (path / '*.fdoc').glob()))
 
-    dsts.extend(buildsystem.copy_flxs_to_libstd(phase.ctx,
-        (path / 'std/*.flx{,h}').glob()))
-    dsts.extend(buildsystem.copy_flxs_to_libstd(phase.ctx,
-        (path / 'std/*.fdoc').glob()))
+    #dsts.extend(buildsystem.copy_flxs_to_libstd(phase.ctx,
+    #    (path / 'std/*.flx{,h}').glob()))
+    #dsts.extend(buildsystem.copy_flxs_to_libstd(phase.ctx,
+    #    (path / 'std/*.fdoc').glob()))
 
-    dsts.extend(buildsystem.copy_flxs_to_libstd_posix(phase.ctx,
-        (path / 'std/posix/*.flx{,h}').glob()))
-    dsts.extend(buildsystem.copy_flxs_to_libstd_posix(phase.ctx,
-        (path / 'std/posix/*.fdoc').glob()))
+    #dsts.extend(buildsystem.copy_flxs_to_libstd_posix(phase.ctx,
+    #    (path / 'std/posix/*.flx{,h}').glob()))
+    #dsts.extend(buildsystem.copy_flxs_to_libstd_posix(phase.ctx,
+    #    (path / 'std/posix/*.fdoc').glob()))
 
     dsts.extend(buildsystem.copy_flxs_to_libstl(phase.ctx,
         (path / 'stl/*.flx{,h}').glob()))
@@ -29,3 +29,4 @@ def build_flx(phase):
         (path / 'stl/*.fdoc').glob()))
 
     return dsts
+
