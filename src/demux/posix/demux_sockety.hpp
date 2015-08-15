320a98484d693a2ee28ab53e7b441b09762a3758
John Skaller <skaller@users.sourceforge.net>
2015-08-16 08:54:35 +1000

    More packaging. Fix visibility bugs.

--- src/demux/posix/demux_sockety.hpp
+++ src/demux/posix/demux_sockety.hpp
@@ -3,25 +3,23 @@
 #include <flx_demux_config.hpp>
 namespace flx { namespace demux {
 
-// Shouldn't this all be DEMUX_EXTERN? eh, not actually compiled on win32
-// we'll probably live.
-int create_listener_socket(int* io_port, int q_len);
-int create_async_listener(int* io_port, int q_len);
-int nice_accept(int listener, int* err);
-int nice_connect(const char* addr, int port);
-int async_connect(const char* addr, int port, int* finished, int* err);
+DEMUX_EXTERN int create_listener_socket(int* io_port, int q_len);
+DEMUX_EXTERN int create_async_listener(int* io_port, int q_len);
+DEMUX_EXTERN int nice_accept(int listener, int* err);
+DEMUX_EXTERN int nice_connect(const char* addr, int port);
+DEMUX_EXTERN int async_connect(const char* addr, int port, int* finished, int* err);
 
 /* handy socket building blocks */
 
-int connect_sock(int s, const char* addr, int port);
+DEMUX_EXTERN int connect_sock(int s, const char* addr, int port);
 
 /* this could possibly do with NIC addr as well as port */
-int bind_sock(int s, int* io_port);
+DEMUX_EXTERN int bind_sock(int s, int* io_port);
 
-int make_nonblock(int s);
-int make_linger(int s, int t);
-int set_tcp_nodelay(int s, int disable_nagle);
-int get_socket_error(int s, int* socket_err);
+DEMUX_EXTERN int make_nonblock(int s);
+DEMUX_EXTERN int make_linger(int s, int t);
+DEMUX_EXTERN int set_tcp_nodelay(int s, int disable_nagle);
+DEMUX_EXTERN int get_socket_error(int s, int* socket_err);
 
 }} // namespace demux, flx
 #endif
