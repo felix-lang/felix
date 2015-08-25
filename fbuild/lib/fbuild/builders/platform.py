94e1463db65f86eb2cd47b0f828f1d57bb064439
John Skaller <skaller@users.sourceforge.net>
2015-08-25 14:22:08 +1000

    extra cygwin uname

--- fbuild/lib/fbuild/builders/platform.py
+++ fbuild/lib/fbuild/builders/platform.py
@@ -29,7 +29,7 @@ archmap = {
     'solaris':   {'posix', 'solaris'},
     'sunos':     {'posix', 'solaris', 'sunos'},
     'cygwin':    {'posix', 'cygwin'},
-    'cygwin_nt-10.0':    {'posix', 'cygwin'},
+    'cygwin_nt-10.0':    {'posix', 'linux'}, # CHEAT
     'nocygwin':  {'posix', 'cygwin', 'nocygwin'},
     'mingw':     {'posix', 'mingw'},
     'windows':   {'windows', 'win32'},
