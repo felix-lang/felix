7475825bb5549404d222bcf7a3f252b3b9fef8e6
John Skaller <skaller@users.sourceforge.net>
2015-09-07 11:37:24 +1000

    Rationalise config.
    Move all platform independent configs into share.
    Note most of these are NOT configs as such, they're
    just dllexport/import controls.

--- src/config/target/flx_glob_config.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __FLX_GLOB_CONFIG_H__
-#define __FLX_GLOB_CONFIG_H__
-#include "flx_rtl_config.hpp"
-#ifdef BUILD_GLOB
-#define GLOB_EXTERN FLX_EXPORT
-#else
-#define GLOB_EXTERN FLX_IMPORT
-#endif
-#endif
