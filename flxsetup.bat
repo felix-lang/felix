dc9ac0c157b3a9faef0d52285316f922f2951a9f
U-turkey\skaller <skaller@users.sourceforge.net>
2015-09-10 03:37:07 +1000

    Fix

--- flxsetup.bat
+++ flxsetup.bat
@@ -1,1 +1,1 @@
-set PATH=c:\cygwin64\home\skaller\felix\build\release\host\bin;c:\cygwin64\home\skaller\felix\host\lib\rtl;%PATH%
+set PATH=c:\cygwin64\home\skaller\felix\build\release\host\bin;c:\cygwin64\home\skaller\felix\build\release\host\lib\rtl;%PATH%
