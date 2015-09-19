c7c116dd2552c30508182e33ba282495ec367623
U-turkey\skaller <skaller@users.sourceforge.net>
2015-09-19 12:10:54 +1000

    Try to fix vssetup.bat

--- vssetup.bat
+++ vssetup.bat
@@ -1,1 +1,1 @@
- "c:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+%comspec% /k ""C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"" x86_amd64\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
