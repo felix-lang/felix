453847e7e13c47e6142e540a58f4a661e055d5ce
John Skaller <skaller@users.sourceforge.net>
2015-10-30 20:22:26 +1100

    New version 2015.10.30-rc5

--- buildsystem/version.py
+++ buildsystem/version.py
@@ -1,5 +1,5 @@
 flx_version_major = "2015"
 flx_version_minor = "10"
 flx_version_patch = "30"
-flx_version_release = "-rc3"
+flx_version_release = "-rc5"
 flx_version = flx_version_major+"."+flx_version_minor+"."+flx_version_patch+flx_version_release
