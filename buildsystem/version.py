396c1d020343e95a094e9407192566497214247f
John Skaller <skaller@users.sourceforge.net>
2015-10-30 22:53:09 +1100

    New version 2015.10.30-rc9

--- buildsystem/version.py
+++ buildsystem/version.py
@@ -1,5 +1,5 @@
 flx_version_major = "2015"
 flx_version_minor = "10"
 flx_version_patch = "30"
-flx_version_release = "-rc8"
+flx_version_release = "-rc9"
 flx_version = flx_version_major+"."+flx_version_minor+"."+flx_version_patch+flx_version_release
