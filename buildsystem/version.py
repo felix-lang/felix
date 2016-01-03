376e4c5834e62b401b7c1f23c81e47da92c755ee
John Skaller <skaller@users.sourceforge.net>
2016-01-04 10:33:07 +1100

    New version 2016.01.04-rc2

--- buildsystem/version.py
+++ buildsystem/version.py
@@ -1,5 +1,5 @@
 flx_version_major = "2016"
 flx_version_minor = "01"
 flx_version_patch = "04"
-flx_version_release = "-rc1"
+flx_version_release = "-rc2"
 flx_version = flx_version_major+"."+flx_version_minor+"."+flx_version_patch+flx_version_release
