0673b454c9d94314be49082b530b946837a7b173
John Skaller <skaller@users.sourceforge.net>
2016-06-01 12:14:34 +1000

    Add generic _map function.

---
+++ src/compiler/flx_bind/flx_gmap.ml
@@ -0,0 +1,30 @@
+open Flx_ast
+open Flx_btype
+open Flx_types
+
+let apl (sr:Flx_srcref.t) (fn : string) (arg:expr_t) =
+  EXPR_apply ( sr, ( EXPR_name (sr,fn,[]), arg))
+
+let rec generic_map bsym_table state be rs sr env fn b =
+  let argx,argt as arg = be rs b in
+  match argt with
+  | BTYP_tuple ls ->
+    let ints = Flx_list.nlist (List.length ls) in
+    let xs = List.map (fun i ->  EXPR_get_n (sr,(i,b))) ints in
+    let xs = List.map (apl sr fn) xs in
+    let e = EXPR_tuple (sr,xs) in
+    be rs e
+
+  | BTYP_array (t,_) ->
+    (* print_endline ("Cheating, array element type " ^ Flx_print.sbt bsym_table t); *)
+    let ubt = Flx_typecode_of_btype.typecode_of_btype bsym_table sr t in
+    let fname = EXPR_suffix(sr, (`AST_name (sr,fn,[]),ubt)) in
+    be rs (EXPR_apply (sr, (EXPR_apply (sr,(EXPR_name (sr,"map",[]),fname)),b)))
+
+  | _ -> 
+    be rs (EXPR_apply (sr, (EXPR_apply (sr,(EXPR_name (sr,"map",[]),EXPR_name (sr,fn,[]))),b)))
+
+(*
+   Flx_exceptions.clierr sr ("Generic _map "^fn^
+    " requires tuple argument, got " ^ Flx_print.sbt bsym_table argt)
+*)
