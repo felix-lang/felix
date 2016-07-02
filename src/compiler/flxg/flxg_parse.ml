8fab885d23bd1ab26c510135a3723b42d820e572
Garrett Bluma <garrett.bluma@gmail.com>
2016-07-02 19:11:14 +0000

    Adding some comments and cleaning up formatting.

--- src/compiler/flxg/flxg_parse.ml
+++ src/compiler/flxg/flxg_parse.ml
@@ -3,63 +3,82 @@ open Flx_mtypes2
 open Flxg_state
 
 
-(** Parse an implementation file *)
+(** Parse an implementation file. *)
 let parse_syntax state =
   let include_dirs = state.syms.compiler_options.include_dirs in
-  let synfiles = List.concat
-    (List.map
-      (Flx_colns.render include_dirs)
-      state.syms.compiler_options.syntax)
-  in
-  (* print_endline ("//Parsing syntax " ^ String.concat ", " synfiles); *)
-  let parser_state = List.fold_left
-    (fun state file -> 
-      let hash_includes, local_data = 
-        Flx_parse_driver.parse_file_with_syntax_unit ~include_dirs state file
-      in local_data
-    )
-    (Flx_parse_driver.make_parser_state ())
-    (synfiles)
+
+  (* Get native file paths for each path in include directory. *)
+  let synfiles = 
+    List.concat
+      (List.map
+        (Flx_colns.render include_dirs)
+        state.syms.compiler_options.syntax)
   in
 
-  let auto_imports = List.concat (List.map
-    (Flx_colns.render include_dirs)
-    state.syms.compiler_options.auto_imports)
+  (* Do the parsing. *)
+(* print_endline ("//Parsing syntax " ^ String.concat ", " synfiles); *)
+  let parser_state = 
+    List.fold_left
+      (fun state file -> 
+        let hash_includes, local_data = 
+          Flx_parse_driver.parse_file_with_syntax_unit ~include_dirs state file
+        in local_data)
+      (Flx_parse_driver.make_parser_state ())
+      synfiles
   in
 
-  let parser_state = List.fold_left
-    (fun state file -> 
-      let hash_includes, local_data =
-        Flx_parse_driver.parse_file_with_compilation_unit ~include_dirs state file
-      in local_data
-    )
-    parser_state
-    auto_imports
+  (* From the parsing state, take the resultant includes (auto imports) and get native paths of them. *)
+  let auto_imports = 
+    List.concat (
+      List.map
+        (Flx_colns.render include_dirs)
+        state.syms.compiler_options.auto_imports)
   in
 
+  (* Continue compiling the includes specified in the auto imports. *)
+  let parser_state = 
+    List.fold_left
+      (fun state file -> 
+        let hash_includes, local_data =
+          Flx_parse_driver.parse_file_with_compilation_unit ~include_dirs state file
+        in local_data)
+      parser_state
+      auto_imports
+  in
+
+  (* Dereference the parsing device for storage on disk. *)
   let parsing_device =
     !(Flx_parse_data.global_data.Flx_token.parsing_device)
   in
+
+  (* Debugging. *)
   if state.syms.Flx_mtypes2.compiler_options.Flx_options.print_flag then
   print_endline "PARSED SYNTAX/IMPORT FILES.";
 
-    let filename =state.syms.compiler_options.automaton_filename  in
-    Flx_filesys.mkdirs (Filename.dirname filename);
-print_endline ("Trying to store automaton " ^ filename);
+  (* Create directory for housing the automotons. *)
+  let filename = state.syms.compiler_options.automaton_filename  
+  in Flx_filesys.mkdirs (Filename.dirname filename);
+
+  (* Debugging *)
+  print_endline ("Trying to store automaton " ^ filename);
+
+  (* Open file for binary output of automoton. Saving parsed file objects for later. *)
   let oc = 
     try Some ( open_out_bin filename)
     with _ -> None
   in
   begin match oc with
   | Some oc ->
-    Marshal.to_channel oc parser_state [];
-    Marshal.to_channel oc parsing_device [];
-    close_out oc;
-    print_endline "Saved automaton to disk.";
+      (* If we have a file, output parser state to disk. *)
+      Marshal.to_channel oc parser_state [];
+      Marshal.to_channel oc parsing_device [];
+      close_out oc;
+      print_endline "Saved automaton to disk.";
   | None ->
-    print_endline ("Can't store automaton to disk file " ^ filename);
-  end
-  ;
+      print_endline ("Can't store automaton to disk file " ^ filename);
+  end;
+
+  (* end function, return parser state *)
   parser_state
 
 let load_syntax state =
@@ -67,20 +86,20 @@ let load_syntax state =
     Flx_profile.call "Flxg_parse.parse_syntax" parse_syntax state
   else
   let filename = state.syms.compiler_options.automaton_filename in
-  try
+    try
 (* print_endline ("Trying to load automaton " ^ filename); *)
-     let oc = open_in_bin filename in
-     let local_data = Marshal.from_channel oc in
-     let parsing_device = Marshal.from_channel oc in
-     close_in oc;
-     (* print_endline "Loaded automaton from disk"; *)
-     let env = Flx_parse_data.global_data.Flx_token.env in
-     let scm = local_data.Flx_token.scm in
-     Flx_dssl.load_scheme_defs env scm;
-     Flx_parse_data.global_data.Flx_token.parsing_device := parsing_device;
-     local_data
-  with _ ->
-    print_endline ("Can't load automaton '"^filename^"'from disk: building!");
+       let oc = open_in_bin filename in
+       let local_data = Marshal.from_channel oc in
+       let parsing_device = Marshal.from_channel oc in
+       close_in oc;
+       (* print_endline "Loaded automaton from disk"; *)
+       let env = Flx_parse_data.global_data.Flx_token.env in
+       let scm = local_data.Flx_token.scm in
+       Flx_dssl.load_scheme_defs env scm;
+       Flx_parse_data.global_data.Flx_token.parsing_device := parsing_device;
+       local_data
+    with _ ->
+      print_endline ("Can't load automaton '"^filename^"'from disk: building!");
 
     Flx_profile.call "Flxg_parse.parse_syntax" parse_syntax state
 
