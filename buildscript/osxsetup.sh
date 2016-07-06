b433636f70cafd081717b7c193f21b8d11ddcffb
fuzzthink <john@johnleung.com>
2016-07-06 12:04:15 -0700

    Fixed unable to locate dyld lib during build
    Error due to path having colon at end if var wasn't defined

--- buildscript/osxsetup.sh
+++ buildscript/osxsetup.sh
@@ -1,3 +1,2 @@
 export PATH=build/release/host/bin:$PATH
-export DYLD_LIBRARY_PATH=build/release/host/lib/rtl:$DYLD_LIBRARY_PATH
-
+export DYLD_LIBRARY_PATH=build/release/host/lib/rtl${DYLD_LIBRARY_PATH:+:}${DYLD_LIBRARY_PATH:-}
