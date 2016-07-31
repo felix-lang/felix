b09fd95f0b4cc08d7fd1c6e143c05215aa58e5a0
John Skaller <skaller@users.sourceforge.net>
2016-08-01 00:17:05 +1000

    Add technotes. [ci skip]

--- makedoc.sh
+++ makedoc.sh
@@ -4,3 +4,11 @@ makeindex felix-ref
 makeglossaries felix-ref
 pdflatex -shell-escape src/docs/felix-ref.tex
 cp felix-ref.pdf ~/Desktop
+pdflatex -shell-escape src/docs/technote-compact-linear-types.tex
+pdflatex -shell-escape src/docs/technote-compact-linear-types.tex
+cp technote-compact-linear-types.pdf ~/Desktop
+pdflatex -shell-escape src/docs/technote-row-polymorphism.tex
+pdflatex -shell-escape src/docs/technote-row-polymorphism.tex
+cp technote-row-polymorphism.pdf ~/Desktop
+
+
