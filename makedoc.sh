3b8f2b604d0550357ef9c572ee2d10a66fc922ce
John Skaller <skaller@users.sourceforge.net>
2016-08-29 11:32:36 +1000

    Add new paper on the evils of functional programming.

--- makedoc.sh
+++ makedoc.sh
@@ -4,11 +4,17 @@ makeindex felix-ref
 makeglossaries felix-ref
 pdflatex -shell-escape src/docs/felix-ref.tex
 cp felix-ref.pdf ~/Desktop
+#
 pdflatex -shell-escape src/docs/technote-compact-linear-types.tex
 pdflatex -shell-escape src/docs/technote-compact-linear-types.tex
 cp technote-compact-linear-types.pdf ~/Desktop
+#
 pdflatex -shell-escape src/docs/technote-row-polymorphism.tex
 pdflatex -shell-escape src/docs/technote-row-polymorphism.tex
 cp technote-row-polymorphism.pdf ~/Desktop
+#
+pdflatex -shell-escape src/docs/ci.tex
+pdflatex -shell-escape src/docs/ci.tex
+cp ci.pdf ~/Desktop
 
 
