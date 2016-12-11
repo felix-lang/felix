dbbb3c27b2d36be1681d176dbf67726ef8d9c5e4
John Skaller <skaller@users.sourceforge.net>
2016-12-11 21:00:02 +1100

    iphone toolchain support

--- buildsystem/plugins.py
+++ buildsystem/plugins.py
@@ -27,6 +27,8 @@ def build(phase, felix):
       "toolchain_gcc_osx",
       "toolchain_gcc_linux",
       "toolchain_msvc_win32",
+      "toolchain_iphoneos",
+      "toolchain_iphonesimulator",
       "flx_plugin",
       ]
     for base in plugins:
