91f3069cc25f2af3c8c35ea0e684a576545cdabf
John Skaller <skaller@users.sourceforge.net>
2017-08-20 16:29:35 +1000

    Add initial support for once check in subroutines.
    Only procedures, only one level.

--- src/compiler/flx_frontend/flx_once.ml
+++ src/compiler/flx_frontend/flx_once.ml
@@ -6,7 +6,28 @@ open Flx_bexe
 open Flx_bexpr
 
 type once_data_t = {gets: BidSet.t; sets: BidSet.t}
+type augexe_t = Flx_bexe.t * once_data_t 
 
+let make_augexes bsym_table once_kids get_sets get_gets bexes : augexe_t list=
+  List.map 
+  (
+    fun bexe -> 
+      bexe, 
+      {
+        sets=get_sets bsym_table once_kids bexe; 
+        gets=get_gets bsym_table once_kids bexe
+      }
+  ) 
+  bexes 
+
+
+
+module BidSetSet = Flx_set.Make (
+  struct 
+    type t = BidSet.t
+    let compare = compare
+  end
+)
 
 (* return the instructions following a label
   if the label isn't found, well fine, there are
@@ -20,18 +41,32 @@ let rec find_label augexes lab =
 
 (* Fixup diagnostics later *)
 let live old {sets=set; gets=get} =
-  if not (BidSet.is_empty (BidSet.inter old set)) then begin
-    print_endline ("Once error: Resetting live variable");
-    assert false;
-  end;
+(*
+print_endline ("Calculate liveness: old= " ^ Flx_bid.str_of_bidset old ^ ", gets=" ^
+  Flx_bid.str_of_bidset get ^ " set=" ^ str_of_bidset set);
+*)
 
-  let newlive = BidSet.union old set in
-  if not (BidSet.subset get newlive) then begin
+  (* Order matters, check gets on old live values first *)
+  if not (BidSet.subset get old) then begin
     print_endline ("Once error: Using uninitialised or already used once variable");
     assert false;
   end;
 
-  BidSet.diff newlive get
+  (* kill used variables *)
+  let newlive =BidSet.diff old get in 
+
+  (* check we're not setting an already live variable *)
+  if not (BidSet.is_empty (BidSet.inter newlive set)) then begin
+    print_endline ("Once error: Resetting live variable");
+    assert false;
+  end;
+
+  (* Add newly set variables *)
+  let live = BidSet.union newlive set  in
+(*
+print_endline ("Newlive= " ^ Flx_bid.str_of_bidset live);
+*)
+  live
 
 let check_liveness liveset =
   if not (BidSet.is_empty liveset) then begin
@@ -46,8 +81,82 @@ let check_reentry previous current label =
     assert false
   end
 
+(* this is basically a copy of the mainline flow routine,
+however we have to handle a few things differently.
+
+First note, child_flow is interested ONLY in the parent's
+once variables, not those defined in the child itself!
+
+When we hit a return we just return the liveness set,
+we don't check it because it need not be empty.
+
+In fact, there could be multiple possible liveness sets,
+so we have to actually return a set of liveness sets,
+the caller then processes each as a distinct control
+flow path.
+
+*)
+
+let add_liveness liveset final_livesets = 
+  BidSetSet.add liveset final_livesets 
+
+let merge_liveness livesets1 livesets2 = 
+  BidSetSet.union livesets1 livesets2 
+
+let rec child_flow bsym_table visited liveset final_livesets augexes : BidSetSet.t =
+(*
+print_endline ("CHILD FLOW: " ^ (match augexes with | []-> "END" 
+  | (bexe,{sets=sets; gets=gets}) ::_ -> Flx_print.string_of_bexe bsym_table 0 bexe ^ 
+  "\nInput liveness= " ^ Flx_bid.str_of_bidset liveset^
+  "\nSets= " ^ Flx_bid.str_of_bidset sets ^ ", gets= " ^ Flx_bid.str_of_bidset gets));
+*)
+  let child_flow visited liveset final_livesets augexes = child_flow bsym_table visited liveset final_livesets augexes in
+  match augexes with
+  (* exists without usage *)
+  | [] 
+  | (BEXE_proc_return _,_)::_ ->
+    add_liveness liveset final_livesets
+
+  (* exists with possible usage *)
+  | (BEXE_jump_direct (_,_,_,_),deltalife)::_ 
+  | (BEXE_jump (_,_,_),deltalife)::_ 
+  | (BEXE_nonreturn_code (_,_,_),deltalife)::_ 
+  | (BEXE_fun_return (_,_),deltalife)::_ ->
+    let liveset = live liveset deltalife in
+    add_liveness liveset final_livesets
+
+  | (BEXE_goto (_,lidx),_)::_ ->
+    if List.mem_assoc lidx visited then begin
+      check_reentry (List.assoc lidx visited) liveset lidx;
+      add_liveness liveset final_livesets
+    end else 
+      child_flow ((lidx,liveset)::visited) liveset final_livesets (find_label augexes lidx)
+
+  | (BEXE_ifgoto (_,c,lidx),deltalife)::tail ->
+    let liveset = live liveset deltalife in
+
+    let skipped = child_flow visited liveset final_livesets tail in
+    if List.mem_assoc lidx visited then begin
+      check_reentry (List.assoc lidx visited) liveset lidx;
+      add_liveness liveset final_livesets
+    end else
+      let taken = 
+        child_flow ((lidx,liveset)::visited) liveset final_livesets (find_label augexes lidx)
+      in 
+      merge_liveness skipped taken
+      
+  | (BEXE_label (_,lidx),_)::tail ->
+    child_flow ((lidx,liveset)::visited) liveset final_livesets tail
+
+  | (_,deltalife)::tail -> 
+    let liveset = live liveset deltalife in
+    child_flow visited liveset final_livesets tail
+
+
 (* flow analysis *)
-let rec flow visited liveset augexes =
+let rec flow make_augexes bsym_table idx visited liveset augexes =
+  let flow visited liveset augexes = flow make_augexes bsym_table idx visited liveset augexes in
+
   match augexes with
   (* exists without usage *)
   | [] 
@@ -59,6 +168,9 @@ let rec flow visited liveset augexes =
   | (BEXE_jump (_,_,_),deltalife)::_ 
   | (BEXE_nonreturn_code (_,_,_),deltalife)::_ 
   | (BEXE_fun_return (_,_),deltalife)::_ ->
+(*
+print_endline ("DETECTED JUMP or noret code or fun ret");
+*)
     let liveset = live liveset deltalife in
     check_liveness liveset
 
@@ -79,10 +191,39 @@ let rec flow visited liveset augexes =
   | (BEXE_label (_,lidx),_)::tail ->
     flow ((lidx,liveset)::visited) liveset tail
 
+  | (BEXE_call (_,(BEXPR_closure(pidx,_),_),_),deltalife)::tail
+  | (BEXE_call_stack (_,pidx,_,_),deltalife)::tail 
+  | (BEXE_call_direct (_,pidx,_,_),deltalife)::tail ->
+(*
+print_endline ("DETECTED PROCEDURE CALL");
+*)
+    let liveset = live liveset deltalife in
+    let parent,bsym = Flx_bsym_table.find_with_parent bsym_table pidx in
+    begin match parent with
+    | Some idx2 when idx = idx2 ->
+      let bbdcl = Flx_bsym.bbdcl bsym in
+      begin match bbdcl with
+      | BBDCL_fun (prop, bvs, ps, res, effects, bexes) ->
+        let augexes = make_augexes bexes in 
+        let final_livesets = 
+          child_flow bsym_table visited liveset BidSetSet.empty augexes     
+        in 
+        let f liveset = flow visited liveset tail in
+        BidSetSet.iter f final_livesets
+
+      (* must  be external function *)
+      | _ -> flow visited liveset tail
+      end
+    | _ -> flow visited liveset tail
+    end
+
   (* FIXME: temporary hack for concept testing, handle all the
     other cases now!
   *)
-  | (_,deltalife)::tail -> 
+  | (bexe,deltalife)::tail -> 
+(*
+print_endline ("Normall processing for " ^ Flx_print.string_of_bexe bsym_table 0 bexe);
+*)
     let liveset = live liveset deltalife in
     flow visited liveset tail
 
@@ -95,7 +236,9 @@ let is_once bsym_table bid =
 let get_sets bsym_table once_kids bexe =
   let bidset = ref BidSet.empty in
   let add i = 
+(*
     print_endline ("Adding once set " ^ string_of_int i); 
+*)
     if BidSet.mem i !bidset then begin
       print_endline ("Once variable set twice in instruction " ^ Flx_print.string_of_bexe bsym_table 0 bexe);
       assert false
@@ -112,7 +255,9 @@ let get_sets bsym_table once_kids bexe =
 let get_gets bsym_table once_kids bexe = 
   let bidset = ref BidSet.empty in
   let add i = 
+(*
     print_endline ("Adding once get " ^ string_of_int i); 
+*)
     if BidSet.mem i !bidset then begin
       print_endline ("Once variable used twice in instruction " ^ Flx_print.string_of_bexe bsym_table 0 bexe);
       assert false
@@ -129,30 +274,26 @@ let get_gets bsym_table once_kids bexe =
   !bidset
 
 let once_check bsym_table bid name once_kids bexes = 
+(*
   print_endline ("Detected once variable of function " ^ name);
+*)
   let is_once bid = is_once bsym_table bid in
-  let bexes = 
-     List.map 
-     (
-       fun bexe -> 
-         bexe, 
-         {
-           sets=get_sets bsym_table once_kids bexe; 
-           gets=get_gets bsym_table once_kids bexe
-         }
-     ) 
-     bexes 
-   in
-   print_endline ("Calculated once use per instruction of " ^ name ^ ":" ^ string_of_int bid);
-   let bparams = Flx_bsym_table.find_bparams bsym_table bid in 
-   let ps =  List.filter is_once (Flx_bparameter.get_bids (fst bparams)) in
-   let once_params = BidSet.of_list ps in
-   if not (BidSet.is_empty once_params) then 
-     print_endline ("Once parameter starts initialised");
-
-   flow [] once_params bexes;
-   print_endline ("Flow of " ^ name ^ ":" ^ string_of_int bid ^ " checked")
-
+  let make_augexes bexes = make_augexes bsym_table once_kids get_sets get_gets bexes in
+  let bexes = make_augexes bexes in
+(*
+  print_endline ("Calculated once use per instruction of " ^ name ^ ":" ^ string_of_int bid);
+*)
+  let bparams = Flx_bsym_table.find_bparams bsym_table bid in 
+  let ps =  List.filter is_once (Flx_bparameter.get_bids (fst bparams)) in
+  let once_params = BidSet.of_list ps in
+  if not (BidSet.is_empty once_params) then 
+(*
+    print_endline ("Once parameter starts initialised");
+*)
+  flow make_augexes bsym_table bid [] once_params bexes
+(*
+  print_endline ("Flow of " ^ name ^ ":" ^ string_of_int bid ^ " checked")
+*)
 
 let once_bsym bsym_table bid parent bsym =
   let bbdcl = Flx_bsym.bbdcl bsym in
