990c2870ccb7868e20e38f8590b2b79a17c9a452
Garrett Bluma <garrett.bluma@gmail.com>
2017-10-19 16:56:00 -0500

    Cleaning up root folder (moving pdfs)

--- makedoc.sh
+++ makedoc.sh
@@ -1,39 +1,39 @@
-pdflatex -shell-escape src/docs/felix-ref.tex
-pdflatex -shell-escape src/docs/felix-ref.tex
+cd docs
+pdflatex -shell-escape ../src/docs/felix-ref.tex
+pdflatex -shell-escape ../src/docs/felix-ref.tex
 makeindex felix-ref
 makeglossaries felix-ref
-pdflatex -shell-escape src/docs/felix-ref.tex
+pdflatex -shell-escape ../src/docs/felix-ref.tex
 cp felix-ref.pdf ~/Desktop
 #
-pdflatex -shell-escape src/docs/technote-compact-linear-types.tex
-pdflatex -shell-escape src/docs/technote-compact-linear-types.tex
+pdflatex -shell-escape ../src/docs/technote-compact-linear-types.tex
+pdflatex -shell-escape ../src/docs/technote-compact-linear-types.tex
 cp technote-compact-linear-types.pdf ~/Desktop
 #
-pdflatex -shell-escape src/docs/technote-row-polymorphism.tex
-pdflatex -shell-escape src/docs/technote-row-polymorphism.tex
+pdflatex -shell-escape ../src/docs/technote-row-polymorphism.tex
+pdflatex -shell-escape ../src/docs/technote-row-polymorphism.tex
 cp technote-row-polymorphism.pdf ~/Desktop
 #
-pdflatex -shell-escape src/docs/ci.tex
-pdflatex -shell-escape src/docs/ci.tex
+pdflatex -shell-escape ../src/docs/ci.tex
+pdflatex -shell-escape ../src/docs/ci.tex
 cp ci.pdf ~/Desktop
 
-pdflatex -shell-escape src/docs/coroutines.tex
-pdflatex -shell-escape src/docs/coroutines.tex
+pdflatex -shell-escape ../src/docs/coroutines.tex
+pdflatex -shell-escape ../src/docs/coroutines.tex
 cp coroutines.pdf ~/Desktop
 #
-pdflatex -shell-escape src/docs/modern_programming.tex
-pdflatex -shell-escape src/docs/modern_programming.tex
+pdflatex -shell-escape ../src/docs/modern_programming.tex
+pdflatex -shell-escape ../src/docs/modern_programming.tex
 makeindex modern_programming
 makeglossaries modern_programming
-pdflatex -shell-escape src/docs/modern_programming.tex
+pdflatex -shell-escape ../src/docs/modern_programming.tex
 cp modern_programming.pdf ~/Desktop
 
 #
-pdflatex -shell-escape src/docs/coprogramming.tex
-pdflatex -shell-escape src/docs/coprogramming.tex
+pdflatex -shell-escape ../src/docs/coprogramming.tex
+pdflatex -shell-escape ../src/docs/coprogramming.tex
 makeindex coprogramming
 makeglossaries coprogramming
-pdflatex -shell-escape src/docs/coprogramming.tex
+pdflatex -shell-escape ../src/docs/coprogramming.tex
 cp coprogramming.pdf ~/Desktop
-
-
+cd -
