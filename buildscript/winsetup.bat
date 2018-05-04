bce6ab8c9d78f7f136c960e918823672eb24d397
John Skaller <skaller@users.sourceforge.net>
2018-05-05 00:08:54 +1000

    fix reference to pushp to work from top level repo

--- buildscript/winsetup.bat
+++ buildscript/winsetup.bat
@@ -1,6 +1,5 @@
-call pushp c:\ocaml\bin
-call pushp "c:\Program Files (x86)\flexdll"
-call pushp build\release\host\lib\rtl
-call pushp build\release\host\bin
+call buildscript\pushp c:\ocaml\bin
+call buildscript\pushp build\release\host\lib\rtl
+call buildscript\pushp build\release\host\bin
 set FLX_MIN_MEM=1000
 set PWD=%cd%
