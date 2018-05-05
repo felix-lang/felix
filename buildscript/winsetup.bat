2608098d02c7f7c1e30f08e14057906d57a57a96
skaller <skaller@users.sourceforge.net>
2018-05-06 00:41:04 +1000

    Win32 buildscript

--- buildscript/winsetup.bat
+++ buildscript/winsetup.bat
@@ -1,5 +1,7 @@
 call buildscript\pushp c:\ocaml\bin
+call buildscript\pushp c:\ocaml
 call buildscript\pushp build\release\host\lib\rtl
 call buildscript\pushp build\release\host\bin
+set ocamlib=c:\ocaml\lib
 set FLX_MIN_MEM=1000
 set PWD=%cd%
