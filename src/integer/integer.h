dbef94b8914f69555a8629c97522663f810a030c
skaller <skaller@users.sourceforge.net>
2018-09-25 00:31:13 +1000

    Remove integer because it doesn't build.

--- src/integer/integer.h
+++
@@ -1,9 +0,0 @@
-
-// PUBLIC IMPORT HEADER
-#ifndef _INTEGER_H_
-#define _UINTEGER_H_
-#include "integer_config.include"
-#define INTEGER_EXTERN _INTEGER_IMPORT
-#include "integer.include"
-#endif
-
--- src/integer/tests/testcases/invert.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-#include "integer.h"
-
-TEST(BitWise, invert){
-    EXPECT_EQ(~integer("00000000", 16), 1);
-    EXPECT_EQ(~integer("deadbeef", 16), 0x21524110);
-    EXPECT_EQ(~integer("ffffffff", 16), 0);
-}
