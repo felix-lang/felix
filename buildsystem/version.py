14cec89c5ccdf2c68567af2b93974a9070ddd46f
skaller <skaller@users.sourceforge.net>
2019-01-06 12:02:51 +1100

    New version 2019.01.06

--- buildsystem/version.py
+++ buildsystem/version.py
@@ -1,5 +1,5 @@
-flx_version_major = "2018"
-flx_version_minor = "09"
-flx_version_patch = "16"
+flx_version_major = "2019"
+flx_version_minor = "01"
+flx_version_patch = "06"
 flx_version_release = ""
 flx_version = flx_version_major+"."+flx_version_minor+"."+flx_version_patch+flx_version_release
