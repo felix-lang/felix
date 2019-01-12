658ea1dbb86527089a0630dfa96368ab88f1d6ba
skaller <skaller@users.sourceforge.net>
2019-01-13 04:58:15 +1100

    Add support for clang on Linux64.

--- installscript/linuxsetup.sh
+++ installscript/linuxsetup.sh
@@ -1,2 +1,2 @@
-export PATH=/usr/local/lib/felix/felix-2018.09.16/host/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/lib/felix/felix-2018.09.16/host/lib/rtl:$LD_LIBRARY_PATH
+export PATH=/usr/local/lib/felix/felix-2019.01.06/host/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/lib/felix/felix-2019.01.06/host/lib/rtl:$LD_LIBRARY_PATH
