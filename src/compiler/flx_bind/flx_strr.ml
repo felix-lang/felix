073eefce1fdc8b2b00cd2349281e96c64b5b3fe2
skaller <skaller@users.sourceforge.net>
2020-09-27 05:21:11 +1000

    make _strr work on cstructs.

--- src/compiler/flx_bind/flx_strr.ml
+++ src/compiler/flx_bind/flx_strr.ml
@@ -228,7 +228,8 @@ print_endline ("_strr Variant type " ^ Flx_print.sbt bsym_table t);
 print_endline ("Strr on nominal type");
 *)
       begin match Flx_lookup_state.hfind "lookup:_strr" sym_table i with
-      | { Flx_sym.id=name; Flx_sym.vs=(vs,_); Flx_sym.symdef=Flx_types.SYMDEF_struct ls } -> 
+      | { Flx_sym.id=name; Flx_sym.vs=(vs,_); Flx_sym.symdef=Flx_types.SYMDEF_struct ls } 
+      | { Flx_sym.id=name; Flx_sym.vs=(vs,_); Flx_sym.symdef=Flx_types.SYMDEF_cstruct (ls,_) } -> 
         let first = ref true in
         let e = cats (
           List.fold_left (fun acc (s,_) -> 
