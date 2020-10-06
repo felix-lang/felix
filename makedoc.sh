d6b581ae4ebf772fdd721cc3419542f193d4164c
skaller <skaller@users.sourceforge.net>
2020-10-06 17:29:54 +1100

    Fix makedoc.sh to reflect new subdir names.

--- makedoc.sh
+++ makedoc.sh
@@ -1,48 +1,48 @@
-cd docs
-pdflatex -shell-escape ../src/docs/typesystem.tex
-pdflatex -shell-escape ../src/docs/typesystem.tex
+cd tex
+pdflatex -shell-escape ../src/tex/typesystem.tex
+pdflatex -shell-escape ../src/tex/typesystem.tex
 mv typesystem.pdf ../pdfs
 #
-pdflatex -shell-escape ../src/docs/linkagearch.tex
-pdflatex -shell-escape ../src/docs/linkagearch.tex
+pdflatex -shell-escape ../src/tex/linkagearch.tex
+pdflatex -shell-escape ../src/tex/linkagearch.tex
 mv linkagearch.pdf ../pdfs
 #
-pdflatex -shell-escape ../src/docs/felix-ref.tex
-pdflatex -shell-escape ../src/docs/felix-ref.tex
+pdflatex -shell-escape ../src/tex/felix-ref.tex
+pdflatex -shell-escape ../src/tex/felix-ref.tex
 makeindex felix-ref
 makeglossaries felix-ref
-pdflatex -shell-escape ../src/docs/felix-ref.tex
+pdflatex -shell-escape ../src/tex/felix-ref.tex
 mv felix-ref.pdf ../pdfs
 #
-pdflatex -shell-escape ../src/docs/technote-compact-linear-types.tex
-pdflatex -shell-escape ../src/docs/technote-compact-linear-types.tex
+pdflatex -shell-escape ../src/tex/technote-compact-linear-types.tex
+pdflatex -shell-escape ../src/tex/technote-compact-linear-types.tex
 mv technote-compact-linear-types.pdf ../pdfs
 #
-pdflatex -shell-escape ../src/docs/technote-row-polymorphism.tex
-pdflatex -shell-escape ../src/docs/technote-row-polymorphism.tex
+pdflatex -shell-escape ../src/tex/technote-row-polymorphism.tex
+pdflatex -shell-escape ../src/tex/technote-row-polymorphism.tex
 mv technote-row-polymorphism.pdf ../pdfs
 #
-pdflatex -shell-escape ../src/docs/ci.tex
-pdflatex -shell-escape ../src/docs/ci.tex
+pdflatex -shell-escape ../src/tex/ci.tex
+pdflatex -shell-escape ../src/tex/ci.tex
 mv ci.pdf ../pdfs
 
-pdflatex -shell-escape ../src/docs/coroutines.tex
-pdflatex -shell-escape ../src/docs/coroutines.tex
+pdflatex -shell-escape ../src/tex/coroutines.tex
+pdflatex -shell-escape ../src/tex/coroutines.tex
 mv coroutines.pdf ../pdfs
 #
-pdflatex -shell-escape ../src/docs/modern_programming.tex
-pdflatex -shell-escape ../src/docs/modern_programming.tex
+pdflatex -shell-escape ../src/tex/modern_programming.tex
+pdflatex -shell-escape ../src/tex/modern_programming.tex
 makeindex modern_programming
 makeglossaries modern_programming
-pdflatex -shell-escape ../src/docs/modern_programming.tex
+pdflatex -shell-escape ../src/tex/modern_programming.tex
 mv modern_programming.pdf ../pdfs
 
 #
-#pdflatex -shell-escape ../src/docs/coprogramming.tex
-#pdflatex -shell-escape ../src/docs/coprogramming.tex
+#pdflatex -shell-escape ../src/tex/coprogramming.tex
+#pdflatex -shell-escape ../src/tex/coprogramming.tex
 #makeindex coprogramming
 #makeglossaries coprogramming
-#pdflatex -shell-escape ../src/docs/coprogramming.tex
+#pdflatex -shell-escape ../src/tex/coprogramming.tex
 #mv coprogramming.pdf ../pdfs
 cd ..
 sh parsedoc.sh
